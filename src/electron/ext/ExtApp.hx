0e2046b88f4b23aa8f5e3861d06304a6599d8dea
DavisDevelopment <epikit@gmail.com>
2017-03-23 21:55:16 -0500

    added some event methods to electron.ext.ExtApp

--- src/electron/ext/ExtApp.hx
+++ src/electron/ext/ExtApp.hx
@@ -3,6 +3,8 @@ package electron.ext;
 import haxe.extern.EitherType;
 import haxe.Constraints.Function;
 
+import tannus.sys.*;
+
 #if main_process
 @:jsRequire('electron', 'app')
 #elseif renderer_process
@@ -15,10 +17,22 @@ extern class ExtApp {
 	public static function relaunch(?options : ExtAppRelaunchOptions):Void;
 	public static function isReady():Bool;
 	public static function focus():Void;
-	public static function getAppPath():String;
-	public static function getPath(name : ExtAppNamedPath):String;
+	@:native('getAppPath')
+	public static function _getAppPath():String;
+	@:native('getPath')
+	public static function _getPath(name : ExtAppNamedPath):String;
 	public static function getVersion():String;
 	public static function getName():String;
+
+	inline public static function getAppPath():Path return new Path(_getAppPath());
+	inline public static function getPath(name : ExtAppNamedPath):Path return new Path(_getPath(name));
+
+	inline public static function onReady(f : Void->Void):Void {
+	    on('ready', f);
+	}
+	inline public static function onAllClosed(f : Void->Void):Void {
+	    on('window-all-closed', f);
+	}
 }
 
 typedef ExtAppRelaunchOptions = {
