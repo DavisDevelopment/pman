d27d2c911c19febf9922e6eec273b89bdbc036a1
DavisDevelopment <epikit@gmail.com>
2017-03-23 21:44:08 -0500

    changed the 'volume' field of pman.core.PlayerPlaybackProperties to be computed

--- src/pman/core/PlayerPlaybackProperties.hx
+++ src/pman/core/PlayerPlaybackProperties.hx
@@ -1,5 +1,7 @@
 package pman.core;
 
+using tannus.math.TMath;
+
 class PlayerPlaybackProperties {
 	/* Constructor Function */
 	public function new(speed:Float, volume:Float, shuffle:Bool):Void {
@@ -14,9 +16,15 @@ class PlayerPlaybackProperties {
 		return new PlayerPlaybackProperties(speed, volume, shuffle);
 	}
 
+/* === Computed Instance Fields === */
+
+	public var volume(default, set): Float;
+	private inline function set_volume(v : Float):Float {
+	    return (volume = v.clamp(0.0, 1.0));
+	}
+
 /* === Instance Fields === */
 
 	public var speed : Float;
-	public var volume : Float;
 	public var shuffle : Bool;
 }
