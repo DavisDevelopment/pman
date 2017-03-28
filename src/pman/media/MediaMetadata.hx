725730ac6e1f8acc7421667e7c57c1447c79abd5
DavisDevelopment <epikit@gmail.com>
2017-03-27 19:50:49 -0500

    Fleshed out the pman.media.MediaMetadata class
    Added 'toRaw', 'pullRaw', 'isEmpty' and 'isIncomplete' methods

--- src/pman/media/MediaMetadata.hx
+++ src/pman/media/MediaMetadata.hx
@@ -7,6 +7,7 @@ import tannus.sys.*;
 import pman.core.*;
 import pman.display.media.*;
 import pman.db.*;
+import pman.db.MediaStore;
 import pman.media.MediaType;
 import pman.media.MediaSource;
 
@@ -25,16 +26,54 @@ using pman.media.MediaTools;
 class MediaMetadata {
     /* Constructor Function */
     public function new():Void {
-        duration = 0.0;
+        duration = null;
         video = null;
         audio = null;
     }
 
 /* === Instance Methods === */
 
+    /**
+      * convert [this] to a MediaInfoRowMeta instance
+      */
+    public function toRaw():MediaInfoRowMeta {
+        return {
+            duration: duration,
+            video: video,
+            audio: audio
+        };
+    }
+
+    /**
+      * pull info from a MediaInfoRowMeta object
+      */
+    public function pullRaw(raw : MediaInfoRowMeta):Void {
+        duration = raw.duration;
+        video = untyped raw.video;
+        audio = untyped raw.audio;
+    }
+
+    /**
+      * determine whether [this] metadata object is void of useful data
+      */
+    public function isEmpty():Bool {
+        return (duration == null && video == null && audio == null);
+    }
+
+    /**
+      * determine whether [this] metadata is missing any data
+      */
+    public function isIncomplete():Bool {
+        return (
+            (duration == null) ||
+            (video == null) || 
+            (audio == null)
+        );
+    }
+
 /* === Instance Fields === */
 
-    public var duration : Float;
+    public var duration : Null<Float>;
     public var video : Null<VideoMetadata>;
     public var audio : Null<AudioMetadata>;
 }
