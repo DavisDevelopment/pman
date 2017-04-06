58a6b8dbcc3e73d85af8d233a450f304bc4fc092
DavisDevelopment <epikit@gmail.com>
2017-04-05 20:08:36 -0500

    Updated the Track-based implementation of the search engine

--- src/pman/search/TrackSearchEngine.hx
+++ src/pman/search/TrackSearchEngine.hx
@@ -1,9 +1,20 @@
 package pman.search;
 
 import pman.media.*;
+import pman.media.MediaSource;
 
 class TrackSearchEngine extends SearchEngine<Track> {
 	override function getValue(track : Track):String {
 		return track.title;
+	}
+	override function getValues(track : Track):Array<String> {
+	    var values = [track.title];
+	    switch ( track.source ) {
+            case MSLocalPath( path ):
+                values.push(path.toString());
+            case MSUrl( url ):
+                values.push( url );
+	    }
+	    return values;
 	}
 }
