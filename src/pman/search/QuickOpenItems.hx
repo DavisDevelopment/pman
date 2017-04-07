5f7e2a216279eceb477d47da2e42ec3810168bf2
DavisDevelopment <epikit@gmail.com>
2017-04-07 01:45:55 -0500

    Created new pman.search.QuickOpenItems class

---
+++ src/pman/search/QuickOpenItems.hx
@@ -0,0 +1,60 @@
+package pman.search;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+
+import pman.db.AppDir;
+import pman.search.QuickOpenItem;
+import pman.media.MediaSource;
+
+import electron.ext.App;
+import electron.Tools.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda; 
+using pman.media.MediaTools;
+
+@:expose( 'qoi' )
+class QuickOpenItems {
+    /**
+      * asynchronously fetch all available QuickOpenItems
+      */
+    public static function get(f : Array<QuickOpenItem>->Void):Void {
+        var results:Array<QuickOpenItem> = new Array();
+        var stack = new AsyncStack();
+        var ad = new AppDir();
+        inline function d(f) stack.push( f );
+        inline function qoi(item) results.push( item );
+
+        d(function(next) {
+            defer(function() {
+                var spln = ad.allSavedPlaylistNames();
+                for (name in spln) {
+                    qoi(QOPlaylist( name ));
+                }
+                next();
+            });
+        });
+
+        d(function(next) {
+            ad.getMediaSources(function(mspaths) {
+                var msdirs = mspaths.map.fn(new Directory( _ ));
+                msdirs.igetAllOpenableFiles(function( files ) {
+                    for (file in files) {
+                        //qoi(QOFile( file ));
+                        qoi(QOMedia(MSLocalPath( file.path )));
+                    }
+                    next();
+                });
+            });
+        });
+
+        stack.run(function() {
+            f( results );
+        });
+    }
+}
