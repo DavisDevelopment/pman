2b8caa4050f4587983854569503a21131d61cbec
DavisDevelopment <epikit@gmail.com>
2017-04-07 01:45:40 -0500

    Created new pman.search.QoSearchEngine
    Implementation of the pman.search.SearchEngine class for searching QuickOpenItems

---
+++ src/pman/search/QoSearchEngine.hx
@@ -0,0 +1,22 @@
+package pman.search;
+
+import pman.media.*;
+import pman.media.MediaSource;
+import pman.search.QuickOpenItem;
+
+class QoSearchEngine extends SearchEngine<QuickOpenItem> {
+    override function getValues(item : QuickOpenItem):Array<String> {
+        switch ( item ) {
+            case QOMedia( src ):
+                switch ( src ) {
+                    case MSLocalPath( path ):
+                        return path.pieces;
+                    case MSUrl( url ):
+                        return [url];
+                }
+
+            case QOPlaylist( name ):
+                return [name];
+        }
+    }
+}
