54f80afbc94ed21f9c8c0a197f2c490321aa4894
DavisDevelopment <epikit@gmail.com>
2017-04-11 21:43:00 -0500

    Created new pack.LinuxApp class

---
+++ src/packer/pack/LinuxApp.hx
@@ -0,0 +1,27 @@
+package pack;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+
+import haxe.Json;
+import js.Lib.require;
+
+import pack.*;
+import pack.Tools.*;
+import Packer.TaskOptions;
+import pack.PackStandalone;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using pack.Tools;
+
+class LinuxApp extends PackStandalone {
+    /* Constructor Function */
+    public function new(arch:String, ?opts:Array<PackagerOptions>):Void {
+        super('linux', arch, opts);
+    }
+}
