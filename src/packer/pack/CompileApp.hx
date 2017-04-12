8b10f3b23a5f226176b34660d74e242b1777c337
DavisDevelopment <epikit@gmail.com>
2017-04-11 20:36:05 -0500

    Created new pack/CompileApp class for compiling the haxe code

---
+++ src/packer/pack/CompileApp.hx
@@ -0,0 +1,49 @@
+package pack;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+
+import haxe.Json;
+import js.Lib.require;
+
+import pack.*;
+import pack.Tools.*;
+import Packer.TaskOptions;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using pack.Tools;
+
+class CompileApp extends Task {
+    /* Constructor Function */
+    public function new(o : TaskOptions):Void {
+        super();
+
+        defines = o.app.haxeDefs;
+    }
+
+    override function execute(callback : ?Dynamic->Void):Void {
+        var args:Array<String> = [];
+        for (d in defines) {
+            args.push('-D');
+            args.push( d );
+        }
+        args.push('build.hxml');
+        var cwd:Path = path().resolve( '../src' );
+        shell = new ShellSpawnTask('haxe', args, {
+            cwd: cwd.toString(),
+            stdio: 'inherit'
+        });
+        shell.execute(function(?error : Dynamic) {
+            trace('haxe compiler has finished');
+            callback();
+        });
+    }
+
+    public var defines:Array<String>;
+    public var shell:ShellSpawnTask;
+}
