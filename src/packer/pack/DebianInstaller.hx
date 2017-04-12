6dfe88199ed014f452eb5ca1fb96b0b70e293dfe
DavisDevelopment <epikit@gmail.com>
2017-04-11 20:39:41 -0500

    Created new pack/DebianInstaller class

---
+++ src/packer/pack/DebianInstaller.hx
@@ -0,0 +1,32 @@
+package pack;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+
+import haxe.Json;
+import js.Lib.require;
+
+import pack.*;
+import pack.Tools.*;
+import Packer.TaskOptions;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using pack.Tools;
+
+class DebianInstaller extends Installer {
+    /* Constructor Function */
+    public function new(arch:String):Void {
+        super('linux', arch);
+    }
+
+/* === Instance Methods === */
+
+    override function getBuildFunction():Dynamic {
+        return require( 'electron-installer-debian' );
+    }
+}
