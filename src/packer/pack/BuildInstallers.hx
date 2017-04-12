1fd0efd64733ee30e45f9cd63ec5f22ae48e40f4
DavisDevelopment <epikit@gmail.com>
2017-04-11 21:41:44 -0500

    Created pack/BuildInstallers batch for building installers for all specified platforms and architectures

---
+++ src/packer/pack/BuildInstallers.hx
@@ -0,0 +1,44 @@
+package pack;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+
+import haxe.Json;
+import js.Lib.require;
+
+import pack.*;
+import pack.Tools.*;
+import Packer.TaskOptions;
+import pack.PackStandalone as Ps;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using pack.Tools;
+
+class BuildInstallers extends BatchTask {
+    public function new(o : TaskOptions):Void {
+        super();
+
+        inline function q(t:Task)
+            children.push( t );
+
+        for (platform in o.platforms) {
+            for (arch in o.arches) {
+                switch ( platform ) {
+                    case 'linux':
+                        q(new DebianInstaller( arch ));
+
+                    case 'win32', 'windows':
+                        q(new WindowsInstaller( arch ));
+
+                    default:
+                        throw 'Error: $platform platform not yet supported';
+                }
+            }
+        }
+    }
+}
