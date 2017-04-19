903733ff715474d4425400585ee8c8c211fa1441
DavisDevelopment <epikit@gmail.com>
2017-04-19 14:44:17 -0500

    created new pman.ui.PreferencesPage class

---
+++ src/pman/ui/PreferencesPage.hx
@@ -0,0 +1,28 @@
+package pman.ui;
+
+import tannus.io.*;
+import tannus.nw.FileChooser;
+import tannus.html.Element;
+
+import crayon.*;
+import foundation.*;
+
+import gryffin.core.*;
+import gryffin.display.*;
+
+import electron.ext.*;
+import electron.ext.Dialog;
+
+import pman.core.*;
+import pman.media.*;
+
+using StringTools;
+using Lambda;
+using Slambda;
+
+class PreferencesPage extends Page {
+    /* Constructor Function */
+    public function new(app : BPlayerMain):Void {
+        super();
+    }
+}
