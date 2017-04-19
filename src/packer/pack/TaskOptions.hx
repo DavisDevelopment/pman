8701c2dbe231b5e3228779310c67a1189ca0c511
DavisDevelopment <epikit@gmail.com>
2017-04-19 02:41:37 -0500

    converted packer.pack.TaskOptions to a class

--- src/packer/pack/TaskOptions.hx
+++ src/packer/pack/TaskOptions.hx
@@ -14,25 +14,60 @@ using tannus.ds.ArrayTools;
 using Slambda;
 using pack.Tools;
 
-typedef TaskOptions = {
-    release:Bool,
-    compress:Bool,
-    concat:Bool,
-    platforms:Array<String>,
-    arches:Array<String>,
-    styles: {
-        compile: Bool,
-        compress: Bool,
-        concat: Bool
-    },
-    scripts: {
-        compile: Bool,
-        compress: Bool,
-        concat: Bool
-    },
-    app: {
-        compile: Bool,
-        haxeDefs: Array<String>
-    },
-    directives: Array<String>
+@:structInit
+class TaskOptions {
+/* === Instance Methods === */
+
+    /**
+      * check for existence of flag
+      */
+    public function hasFlag(flag : String):Bool {
+        return flags.exists( flag );
+    }
+
+    /**
+      * get the value associated with a given flag
+      */
+    public function getFlag<T>(flag : String):Maybe<T> {
+        return flags[flag];
+    }
+
+    /**
+      * set a flag
+      */
+    public function setFlag<T>(flag:String, value:T):T {
+        return (flags[flag] = value);
+    }
+
+/* === Instance Fields === */
+
+    public var release : Bool;
+    public var compress : Bool;
+    public var concat : Bool;
+    public var platforms : Array<String>;
+    public var arches : Array<String>;
+    public var directives : Array<String>;
+    public var flags : Dict<String, Dynamic>;
+    public var styles : StylesOpts;
+    public var scripts : ScriptsOpts;
+    public var app : AppOpts;
+}
+
+typedef AssetOpts = {
+    compile : Bool,
+    compress : Bool,
+    concat : Bool
+};
+
+typedef StylesOpts = {
+    >AssetOpts,
+};
+
+typedef ScriptsOpts = {
+    >AssetOpts,
+};
+
+typedef AppOpts = {
+    compile : Bool,
+    haxeDefs : Array<String>
 };
