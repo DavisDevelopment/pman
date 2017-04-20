e3b208f08fd9aae8b30844ca6db82cac531eebbe
DavisDevelopment <epikit@gmail.com>
2017-04-20 18:48:01 -0500

    created pman.db.TagsStore table wrapper

--- src/pman/db/TagsStore.hx
+++ src/pman/db/TagsStore.hx
@@ -9,6 +9,8 @@ import ida.backend.idb.IDBCursorWalker in CursorWalker;
 
 import pman.core.*;
 import pman.media.*;
+import pman.async.*;
+import pman.media.info.Tag;
 
 import js.Browser.console;
 import Slambda.fn;
@@ -36,14 +38,22 @@ class TagsStore extends TableWrapper {
     public function putTagRow(row : TagRow):Promise<TagRow> {
         return cast put('tags', row);
     }
-    public function putTagRow_(row:TagRow, f:Null<Dynamic>->Null<TagRow>->Void):Void {
+    public function putTagRow_(row:TagRow, ?done:Cb<TagRow>):Void {
+        if (done == null)
+            done = untyped fn([e=null,v=null]=>null);
         var p = putTagRow( row );
         p.then(function( row ) {
-            f(null, row);
+            done(null, row);
         });
         p.unless(function( error ) {
-            f(error, null);
+            done(error, null);
         });
+    }
+    public function addTag(tag:Tag, ?done:Cb<TagRow>):Void {
+        putTagRow_(tag.toRow(), done);
+    }
+    public function add(name:String, ?type:TagType, ?done:Cb<TagRow>):Void {
+        addTag(new Tag(name, type), done);
     }
 
     /**
@@ -52,58 +62,37 @@ class TagsStore extends TableWrapper {
     public function getTagRow(name : String):Promise<Null<TagRow>> {
         return untyped tos('tags').get( name );
     }
-    public function getTagRow_(name:String, f:Null<Dynamic>->Null<TagRow>->Void):Void {
-        var p = getTagRow( name );
-        p.then(function(row : Null<TagRow>) {
-            f(null, row);
-        });
-        p.unless(function(error : Dynamic) {
-            f(error, null);
-        });
+    public function getTagRow_(name:String, done:Cb<TagRow>):Void {
+        getTagRow( name ).then( done ).unless( done );
+    }
+    public function getTag(name : String):Promise<Null<Tag>> {
+        return getTagRow( name ).transform.fn(_ != null ? Tag.fromRow(_) : null);
+    }
+    public function getTag_(name:String, done:Cb<Tag>):Void {
+        getTag( name ).then(done).unless(done);
     }
 
     /**
       * create or retrieve a row from [this] table
       */
-    public function cogTagRow(name : String):Promise<TagRow> {
+    public function cogTagRow(name:String, ?type:TagType):Promise<TagRow> {
         return Promise.create({
-            getTagRow_(name, function(error:Null<Dynamic>, row:Null<TagRow>) {
-                if (error != null) {
+            getTagRow_(name, function(?error:Dynamic, ?row:TagRow) {
+                if (error != null)
                     throw error;
-                }
+                else if (row != null)
+                    return row;
                 else {
-                    if (row == null) {
-                        @forward putTagRow({
-                            name: name,
-                            type: ''
-                        });
-                    }
-                    else {
-                        return row;
-                    }
+                    add(name, type, function(?error:Dynamic, ?row:TagRow) {
+                        if (error != null)
+                            throw error;
+                        else
+                            return row;
+                    });
                 }
             });
         });
     }
-
-    /**
-      * remove a tag from the database
-      */
-    /*
-    public function deleteTagRow(name:String):BoolPromise {
-        return Promise.create({
-            var store = tos('tags', 'readwrite');
-            store.delete(name, function(error:Null<Dynamic>) {
-                if (error != null) {
-                    throw error;
-                }
-                else {
-
-                }
-            });
-        }).bool();
-    }
-    */
 
 /* === Instance Fields === */
 }
