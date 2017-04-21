803b909e30fae1593253bae30590167e93c2c34c
DavisDevelopment <epikit@gmail.com>
2017-04-21 04:57:30 -0500

    rewrote pman.server.Server to use express. works perfectly now

--- src/pman/server/Server.hx
+++ src/pman/server/Server.hx
@@ -5,23 +5,17 @@ import tannus.ds.*;
 import tannus.ds.tuples.*;
 import tannus.sys.Path;
 import tannus.sys.FileSystem in Fs;
-import tannus.node.Fs as NodeFs;
+import tannus.node.*;
+import tannus.node.Fs as Nfs;
 
-import electron.main.*;
-import electron.main.Menu;
-import electron.main.MenuItem;
-import electron.ext.App;
 import electron.Tools.defer;
-
-import js.html.Window;
 
 import tannus.TSys as Sys;
 
 import pman.db.AppDir;
+import pman.async.*;
 
-import hxpress.*;
-import hxpress.Server as NServer;
-
+import js.Lib.require;
 import haxe.Template;
 
 using StringTools;
@@ -31,123 +25,106 @@ using Lambda;
 using Slambda;
 using pman.server.ServerTools;
 
-class Server extends NServer {
+class Server {
     /* Constructor Function */
-    public function new(main : ServerMain):Void {
-        super();
-
+    public function new(main : Background):Void {
         this.main = main;
+        _cc = new Map();
+        _routes = new Map();
+        _routes['test'] = new Path('/home/ryan/Videos/ass.mp4');
     }
 
 /* === Instance Methods === */
 
-    public function init(data : ServerInitData):Void {
+    /**
+      * initialize [this] server
+      */
+    public function init(?data : ServerInitData):Void {
         this.data = data;
 
-        route('*', function(req, res) {
-            res.headers['server'] = 'pman-server';
-        });
+        app = express();
+        app.get('/watch/:uuid', sendSeekable, _respond.bind());
 
-        var homeTemplate = readTemplate( 'home.html' );
-        route('/home', function(req, res) {
-            res.headers['Content-Type'] = 'text/html';
-            res.status = 200;
-            res.write(homeTemplate.execute({}));            
-            res.send();
-        });
+        app.listen( 6969 );
+    }
 
-        var defaultLength:Int = 24325518;
-        route('/media.mp4', function(req, res) {
-            var r = res.getRaw();
-            var mediaPath:Path = new Path('/home/ryan/Videos/Popcorn/porn/asian-hotties/asian-teen-rough-fuck-double-creampie.mp4');
-            var mediaStat = Fs.stat( mediaPath );
-            var byteRange:Null<IRange> = range( req );
-
-            if (byteRange == null) {
-                byteRange = [0, (mediaStat.size - 1)];
-            }
-
-            if (byteRange.x == null)
-                byteRange.x = 0;
-            if (byteRange.y == null)
-                byteRange.y = (mediaStat.size - 1);
-
-            r.writeHead(206, {
-                'Content-Type': 'video/mp4',
-                'Accept-Ranges': 'bytes',
-                'Content-Length': Std.string( mediaStat.size ),
-                'Content-Range': ('bytes ${byteRange.x}-${byteRange.y}/${mediaStat.size}')
-            });
-
-            var stream = NodeFs.createReadStream(mediaPath.toString(), {
-                start: byteRange.x,
-                end: byteRange.y
-            });
-            stream.onOpen(function() {
-                stream.pipe( r );
-            });
-            stream.onError(function( error ) {
-                r.end(Std.string( error ));
-            });
-        });
-
-        listen( 6969 );
+    public function serve(path : Path):String {
+        for (uid in _routes.keys()) {
+            if (_routes[uid].str == path.str)
+                return uid;
+        }
+        var id = Uuid.create();
+        _routes[id] = path;
+        return id;
     }
 
     /**
-      * extract the content range from the given request
+      * express route handler
       */
-    private function range(req : Request):Null<IRange> {
-        if (req.headers.exists('range')) {
-            var s = req.headers['range'];
-            s = s.after('bytes=');
-            var vals = s.split('-').map( Std.parseInt );
-            return new IRange( vals );
+    private function _respond(req:Dynamic, res:Dynamic, next:Dynamic):Void {
+        var uid:Null<String> = req.params.uuid;
+        if (uid != null && uid.trim().empty()) uid = null;
+        if (uid != null) {
+            if (_routes.exists( uid )) {
+                var c = _ctx( uid );
+                res.sendSeekable(c.stream, {
+                    length: c.size
+                });
+            }
+            else {
+                res.status( 404 ).end();
+            }
         }
         else {
-            return null;
+            res.write('<body>');
+            for (id in _routes.keys()) {
+                var href = './$id';
+                res.write('<a href="$href">$id</a>');
+                res.write('</br>');
+            }
+            res.write('</body>');
         }
     }
 
-    /**
-      * stream the given File onto the given response
-      */
-    private function stream(path:Path, response:Response, done:Void->Void):Void {
-        function step(chunk : ByteArray):Bool {
-            response.write( chunk );
-            return true;
+    private function _ctx(uid:String):Ctx {
+        if (!_cc.exists( uid )) {
+            var path:Path = _routes.get( uid );
+            var context:Ctx = {
+                path: path,
+                size: _size( path ),
+                stream: _stream( path )
+            };
+            return (_cc[uid] = context);
         }
-        function reject( error ) {
-            throw error;
+        else {
+            return _cc[uid];
         }
-        path.chunkedRead(1024, step, done, reject);
     }
 
-    /**
-      * read a template
-      */
-    public function readTemplate(name : String):Template {
-        var path = new Path( data.appPath ).plusString('assets/templates/$name');
-        var tmplText = Fs.read(path.toString());
-        return new Template( tmplText );
+    private function _size(path:Path):Int {
+        return Fs.stat(path.toString()).size;
+    }
+    private function _stream(path : Path):ReadableStream {
+        return Nfs.createReadStream(path.toString());
     }
 
 /* === Instance Fields === */
 
-    public var main : ServerMain;
+    public var main : Background;
     public var data : ServerInitData;
+    public var _routes : Map<String, Path>;
+    public var _cc : Map<String, Ctx>;
+
+    private var app : Dynamic;
+
+/* === Static Fields === */
+
+    private static var express:Dynamic = {require('express');};
+    private static var sendSeekable:Dynamic = {require('send-seekable');};
 }
 
-abstract IRange (Array<Int>) from Array<Int> {
-    public inline function new(a : Array<Int>) {
-        this = a;
-    }
-
-    public var x(get, set):Null<Int>;
-    private inline function get_x() return this[0];
-    private inline function set_x(v) return (this[0] = v);
-
-    public var y(get, set):Null<Int>;
-    private inline function get_y() return this[1];
-    private inline function set_y(v) return (this[1] = v);
-}
+typedef Ctx = {
+    path: Path,
+    size: Int,
+    stream: ReadableStream
+};
