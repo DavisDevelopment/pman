de79bf5d8b03a0cc355be6fee2ca3fffe171ce7c
DavisDevelopment <epikit@gmail.com>
2017-04-25 21:07:05 -0500

    reimplimented pman.tools.chromecast.ExtDevice

--- src/pman/tools/chromecast/ExtDevice.hx
+++ src/pman/tools/chromecast/ExtDevice.hx
@@ -1,29 +1,46 @@
 package pman.tools.chromecast;
 
 import tannus.node.*;
+import pman.async.*;
 
 import haxe.extern.EitherType;
 
 extern class ExtDevice extends EventEmitter {
-	public function play(resource:EitherType<String, MediaDef>, seconds:Float, callback:ErrCb):Void;
-	public function getStatus(callback : Null<Dynamic>->DeviceStatus->Void):Void;
-	public function seekTo(newCurrentTime:Float, callback:ErrCb):Void;
-	public function seek(time:Float, callback:ErrCb):Void;
-	public function pause(f : ErrCb):Void;
-	public function unpause(f : ErrCb):Void;
-	public function setVolume(volume:Float, cb:ErrCb):Void;
-	public function stop(cb : ErrCb):Void;
-	public function setVolumeMuted(muted:Bool, cb:ErrCb):Void;
-	public function close(cb : ErrCb):Void;
+	public function play(resource:EitherType<String, MediaDef>, seconds:Float, callback:VoidCb):Void;
+	public function getStatus(callback : Cb<DeviceStatus>):Void;
+	public function seekTo(newCurrentTime:Float, callback:VoidCb):Void;
+	public function seek(time:Float, callback:VoidCb):Void;
+	public function pause(f : VoidCb):Void;
+	public function unpause(f : VoidCb):Void;
+	public function setVolume(volume:Float, cb:VoidCb):Void;
+	public function stop(cb : VoidCb):Void;
+	public function setVolumeMuted(muted:Bool, cb:VoidCb):Void;
+	public function close(cb : VoidCb):Void;
 
 	public var host : String;
 	public var config : DeviceConfig;
 }
 
-typedef DeviceStatus = Dynamic;
-
-@:callable
-abstract ErrCb (Null<Dynamic> -> Void) from Null<Dynamic>->Void {}
+typedef DeviceStatus = {
+    currentTime:Float,
+    currentItemId:Int,
+    media: {
+        contentId: String,
+        contentType: String,
+        duration:Float
+    },
+    playbackRate: Float,
+    mediaSessionId: Int,
+    playerState: String,
+    repeatMode: String,
+    supportedMediaCommands: Int,
+    videoInfo: {
+        hdrType: String,
+        height: Int,
+        width: Int
+    },
+    volume: {level:Int, muted:Bool}
+};
 
 typedef MediaDef = {
 	url : String,
