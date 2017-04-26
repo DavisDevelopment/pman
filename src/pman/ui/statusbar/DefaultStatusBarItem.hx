b4908089b8089c938109c4a13b9d526cffb66f25
DavisDevelopment <epikit@gmail.com>
2017-04-26 16:46:01 -0500

    created pman.ui.statusbar.DefaultStatusBarItem class

---
+++ src/pman/ui/statusbar/DefaultStatusBarItem.hx
@@ -0,0 +1,83 @@
+package pman.ui.statusbar;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.geom.*;
+import tannus.events.*;
+import tannus.graphics.Color;
+
+import gryffin.core.*;
+import gryffin.display.*;
+
+import pman.core.*;
+import pman.media.*;
+import pman.display.*;
+import pman.display.media.*;
+import pman.ui.ctrl.*;
+
+import tannus.math.TMath.*;
+import gryffin.Tools.*;
+
+import motion.Actuate;
+import motion.easing.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+
+class DefaultStatusBarItem extends StatusBarItem {
+    /* Constructor Function */
+    public function new():Void {
+        super();
+
+        duration = -1;
+        tb = new TextBox();
+        tb.fontFamily = 'Ubuntu';
+        tb.fontSizeUnit = 'px';
+        tb.fontSize = 15;
+        tb.color = new Color(255, 255, 255);
+    }
+
+/* === Instance Methods === */
+
+    /**
+      * render [this]
+      */
+    override function render(stage:Stage, c:Ctx):Void {
+        var tbr = new Rectangle(0, 0, tb.width, tb.height);
+        tbr.centerY = centerY;
+        tbr.x = 0;
+
+        c.drawComponent(tb, 0, 0, tb.width, tb.height, tbr.x, tbr.y, tbr.w, tbr.h);
+    }
+
+    /**
+      * update [this]
+      */
+    override function update(stage : Stage):Void {
+        super.update( stage );
+
+        var oldText = tb.text;
+        if (player.track == null) {
+            tb.text = '';
+        }
+        else {
+            switch ( player.track.source ) {
+                case MediaSource.MSLocalPath( path ):
+                    tb.text = path;
+                case MediaSource.MSUrl( url ):
+                    tb.text = url;
+            }
+        }
+
+        if (oldText != tb.text) {
+            tb.autoScale(null, rect.h, 0.5);
+        }
+    }
+
+/* === Instance Fields === */
+
+    private var tb : TextBox;
+}
