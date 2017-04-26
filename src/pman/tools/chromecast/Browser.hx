4ee91c29a40e653e7284235bd08a60edcbfccb44
DavisDevelopment <epikit@gmail.com>
2017-04-25 21:06:25 -0500

    reimplimented pman.tools.chromecast.Browser

--- src/pman/tools/chromecast/Browser.hx
+++ src/pman/tools/chromecast/Browser.hx
@@ -5,62 +5,19 @@ import tannus.node.*;
 import tannus.io.*;
 import tannus.ds.*;
 
-import pman.tools.chromecast.ExtDevice;
+import pman.async.*;
 
-class Browser {
-	/* Constructor Function */
-	private function new(browser : ExtBrowser):Void {
-		b = browser;
+import Slambda.fn;
 
-		deviceFound = new Signal();
+using Slambda;
 
-		__init();
-	}
+@:forward
+abstract Browser (ExtBrowser) from ExtBrowser {
+    public inline function new() {
+        this = new ExtBrowser();
+    }
 
-/* === Instance Methods === */
-
-	/**
-	  * initialize [this]
-	  */
-	private function __init():Void {
-		// handle extDevice
-		function device_on(extDevice : ExtDevice):Void {
-			var device:Device = new Device( extDevice );
-			deviceFound.call( device );
-		}
-		b.on('deviceOn', device_on);
-	}
-
-	/**
-	  *  destroy [this] Browser
-	  */
-	public function destroy():Void {
-		// de-initialize stuff
-		b.destroy();
-
-		// nullify fields and free up memory
-		b = null;
-	}
-
-	/**
-	  * update the device-list
-	  */
-	public function update():Void {
-		b.update();
-	}
-
-/* === Instance Fields === */
-
-	public var deviceFound : Signal<Device>;
-
-	private var b : ExtBrowser;
-
-/* === Class Methods === */
-
-	/**
-	  * create and return a new Browser
-	  */
-	public static inline function create():Browser {
-		return new Browser(new ExtBrowser());
-	}
+    public function onDevice(handler : Device->Void):Void {
+        this.onDevice(fn(d=>handler(new Device(d))));
+    }
 }
