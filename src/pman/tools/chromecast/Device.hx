e2b0754fa938447ac584cfee447738f16f46002c
DavisDevelopment <epikit@gmail.com>
2017-04-25 21:06:46 -0500

    reimplimented pman.tools.chromecast.Device

--- src/pman/tools/chromecast/Device.hx
+++ src/pman/tools/chromecast/Device.hx
@@ -5,6 +5,8 @@ import tannus.node.*;
 import tannus.io.Getter;
 import tannus.ds.*;
 
+import pman.async.*;
+
 import haxe.extern.EitherType;
 
 import pman.tools.chromecast.ExtDevice;
@@ -15,7 +17,7 @@ class Device {
 	private function new(device : ExtDevice):Void {
 		d = device;
 
-		__init();
+		//__init();
 	}
 
 /* === Instance Methods === */
@@ -32,70 +34,70 @@ class Device {
 	/**
 	  * initiate playback of some media
 	  */
-	public function play(media:EitherType<String, MediaDef>, seconds:Float, callback:ErrCb):Void {
+	public function play(media:EitherType<String, MediaDef>, seconds:Float, callback:VoidCb):Void {
 		d.play(media, seconds, callback);
 	}
 
 	/**
 	  * query the device for it's current status
 	  */
-	public function getStatus(callback : Null<Dynamic>->DeviceStatus->Void):Void {
+	public function getStatus(callback : Cb<DeviceStatus>):Void {
 		d.getStatus( callback );
 	}
 
 	/**
 	  * seek to the given time
 	  */
-	public function seekTo(newCurrentTime:Float, callback:ErrCb):Void {
+	public function seekTo(newCurrentTime:Float, callback:VoidCb):Void {
 		d.seekTo(newCurrentTime, callback);
 	}
 
 	/**
 	  * seek to the given time
 	  */
-	public function seek(time:Float, callback:ErrCb):Void {
+	public function seek(time:Float, callback:VoidCb):Void {
 		d.seek(time, callback);
 	}
 
 	/**
 	  * pause the current media
 	  */
-	public function pause(f : ErrCb):Void {
+	public function pause(f : VoidCb):Void {
 		d.pause( f );
 	}
 
 	/**
 	  * unpause the current media
 	  */
-	public function unpause(f : ErrCb):Void {
+	public function unpause(f : VoidCb):Void {
 		d.unpause( f );
 	}
 
 	/**
 	  * set the volume
 	  */
-	public function setVolume(volume:Float, cb:ErrCb):Void {
+	public function setVolume(volume:Float, cb:VoidCb):Void {
 		d.setVolume(volume, cb);
 	}
 
 	/**
 	  * stop the current media
 	  */
-	public function stop(cb : ErrCb):Void {
+	public function stop(cb : VoidCb):Void {
 		d.stop( cb );
 	}
 
 	/**
 	  * mute/unmute the current media
 	  */
-	public function setVolumeMuted(muted:Bool, cb:ErrCb):Void {
+	public function setVolumeMuted(muted:Bool, cb:VoidCb):Void {
 		d.setVolumeMuted(muted, cb);
 	}
 
 	/**
 	  * close connection to [this] device
 	  */
-	public function close(cb : ErrCb):Void {
+	public function close(cb : VoidCb):Void {
 		d.close( cb );
 	}
 
