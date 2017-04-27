f0b81903236ccfdf5d69bb8ecd5e3c44b430ef6a
DavisDevelopment <epikit@gmail.com>
2017-04-26 23:12:52 -0500

    created parser for pmsh format

---
+++ src/pman/format/pmsh/Expr.hx
@@ -0,0 +1,19 @@
+package pman.format.pmsh;
+
+import tannus.io.*;
+import tannus.ds.*;
+
+import pman.format.pmsh.Token;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+
+enum Expr {
+    ECommand(name:Word, params:Array<Expr>);
+    ESetVar(name:Word, value:Word);
+    EWord(word : Word);
+    EBlock(body : Array<Expr>);
+}
