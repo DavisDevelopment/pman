127e5eee4bed1deafc684ef6ce47f2acde496112
DavisDevelopment <epikit@gmail.com>
2017-05-09 22:33:03 -0500

    created widget for displaying player's fps

---
+++ src/pman/ui/FPSDisplay.hx
@@ -0,0 +1,89 @@
+package pman.ui;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.geom.*;
+import tannus.events.*;
+import tannus.graphics.Color;
+import tannus.math.Percent;
+
+import gryffin.core.*;
+import gryffin.display.*;
+import gryffin.ui.Border;
+
+import pman.core.*;
+import pman.display.*;
+import pman.display.media.*;
+import pman.ui.ctrl.*;
+
+import tannus.math.TMath.*;
+import gryffin.Tools.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+
+class FPSDisplay extends Ent {
+    /* Constructor Function */
+    public function new(player : Player):Void {
+        super();
+
+        this.player = player;
+        frames = new Pair(0, 0);
+        lt = null;
+        tb = new TextBox();
+        tb.color = new Color(34, 245, 51);
+        tb.fontSize = 12;
+    }
+
+/* === Instance Methods === */
+
+    /**
+      * update [this]
+      */
+    override function update(stage : Stage):Void {
+        if (lt == null) {
+            lt = now;
+        }
+        else {
+            if ((now - lt) >= 1000) {
+                frames.right = frames.left;
+                frames.left = 0;
+                lt = now;
+            }
+            else {
+                frames.left++;
+            }
+        }
+
+        tb.text = 'FPS: ${frames.right}';
+    }
+
+    /**
+      * render [this]
+      */
+    override function render(stage:Stage, c:Ctx):Void {
+        c.drawComponent(tb, 0, 0, tb.width, tb.height, x, y, tb.width, tb.height);
+    }
+
+    /**
+      * calculate [this]'s geometry
+      */
+    override function calculateGeometry(r : Rectangle):Void {
+        r = player.view.rect;
+
+        x = (r.x + r.w - tb.width - 10);
+        y = (r.y + 10);
+        w = tb.width;
+        h = tb.height;
+    }
+
+/* === Instance Fields === */
+
+    public var player : Player;
+    private var frames : Pair<Int, Int>;
+    private var lt : Null<Float>;
+    private var tb : TextBox;
+}
