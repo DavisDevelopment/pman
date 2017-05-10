d2fc3b0e5cebac5deba389bcae36ceca3102aba4
DavisDevelopment <epikit@gmail.com>
2017-05-10 00:56:06 -0500

    created new pman.ui.PreferencesPage class

--- src/pman/ui/PreferencesPage.hx
+++ src/pman/ui/PreferencesPage.hx
@@ -1,8 +1,9 @@
 package pman.ui;
 
 import tannus.io.*;
-import tannus.nw.FileChooser;
+import tannus.ds.*;
 import tannus.html.Element;
+import tannus.sys.FileSystem as Fs;
 
 import crayon.*;
 import foundation.*;
@@ -15,6 +16,10 @@ import electron.ext.Dialog;
 
 import pman.core.*;
 import pman.media.*;
+import pman.db.*;
+
+import Std.*;
+import electron.Tools.*;
 
 using StringTools;
 using Lambda;
@@ -24,5 +29,52 @@ class PreferencesPage extends Page {
     /* Constructor Function */
     public function new(app : BPlayerMain):Void {
         super();
+
+        addClass( 'prefeditor' );
+
+        this.app = app;
     }
+
+/* === Instance Methods === */
+
+    /**
+      * when [this] Page opens
+      */
+    override function open(body : Body):Void {
+        super.open( body );
+
+        el.load('preferences.html', null, function(responseText:String, status:String, xhr) {
+            defer(function() {
+                syncFields();
+                trace( fields );
+            });
+        });
+    }
+
+    /**
+      * obtain references to field-inputs
+      */
+    private function syncFields():Void {
+        var df:Object = {};
+        var names = ['autoPlay', 'autoRestore', 'directRender', 'showAlbumArt', 'showSnapshot', 'snapshotPath'];
+        for (name in names) {
+            df[name] = e('#$name');
+        }
+        this.fields = df;
+    }
+
+    private static inline function e(x : Dynamic):Element return new Element( x );
+
+/* === Instance Fields === */
+
+    private var app : BPlayerMain;
+    private var fields : PFields;
 }
+
+private typedef PFields = {
+    autoPlay : Element,
+    autoRestore : Element,
+    directRender : Element,
+    showAlbumArt : Element,
+    snapshotPath : Element
+};
