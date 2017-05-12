ad7189b5b68abfe6e6ef46f050ccc80e6b5c0e9c
DavisDevelopment <epikit@gmail.com>
2017-05-12 02:26:43 -0500

    removed the 'ple' pmbash command

--- src/pman/pmbash/Interp.hx
+++ src/pman/pmbash/Interp.hx
@@ -45,7 +45,6 @@ class Interp extends Interpreter {
             'plclear' => alias('pl clear'),
             'plsave' => alias('pl save'),
             'plexport' => alias('pl export'),
-            'ple' => alias('pl export'),
             'plshuffle' => alias('pl shuffle')
         ];
     }
