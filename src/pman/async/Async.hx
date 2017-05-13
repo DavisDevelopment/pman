2e25daa9591d4b2dd81dc3a3af479c03c0627101
DavisDevelopment <epikit@gmail.com>
2017-05-12 20:27:11 -0500

    added 'promise' method to pman.async.Async

--- src/pman/async/Async.hx
+++ src/pman/async/Async.hx
@@ -1,7 +1,24 @@
 package pman.async;
 
+import tannus.ds.*;
+
 @:callable
 abstract Async<T> (Cb<T>->Void) from Cb<T>->Void {
     public inline function new(f : Cb<T>->Void)
         this = f;
+
+    @:to
+    public function promise():Promise<T> {
+        return new Promise(function(yield, raise) {
+            this(function(?error, ?result) {
+                if (error != null)
+                    return raise( error );
+                else if (result != null)
+                    return yield( result );
+                else {
+                    trace('Promise left unresolved!');
+                }
+            });
+        });
+    }
 }
