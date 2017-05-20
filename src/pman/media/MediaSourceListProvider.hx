81ace5e1edb1ec10f3687983c95b3a32ff812cc5
DavisDevelopment <epikit@gmail.com>
2017-05-20 06:38:08 -0500

    created new pman.media.MediaSourceListProvider

---
+++ src/pman/media/MediaSourceListProvider.hx
@@ -0,0 +1,13 @@
+package pman.media;
+
+import pman.async.*;
+import pman.async.ReadStream;
+
+import pman.media.*;
+import pman.media.MediaSource;
+
+using pman.async.Asyncs;
+
+class MediaSourceListProvider extends ChunkedReadStream<MediaSource> {
+
+}
