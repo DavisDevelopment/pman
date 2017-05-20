0d5599f22f4ff12b5b2fa0ba4fe7f7853824cf1b
DavisDevelopment <epikit@gmail.com>
2017-05-20 11:54:03 -0500

    created new pman.pmbash.commands.RestartCommand

---
+++ src/pman/pmbash/commands/RestartCommand.hx
@@ -0,0 +1,28 @@
+package pman.pmbash.commands;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.TSys as Sys;
+
+import pman.async.*;
+import pman.format.pmsh.*;
+import pman.format.pmsh.Token;
+import pman.format.pmsh.Expr;
+import pman.format.pmsh.Cmd;
+import pman.pmbash.commands.*;
+
+import electron.Tools.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using pman.async.VoidAsyncs;
+
+class ExitCommand extends Command {
+    override function execute(i:Interpreter, args:Array<CmdArg>, done:VoidCb):Void {
+        defer(main.ic.send('Reload'));
+        done();
+    }
+}
