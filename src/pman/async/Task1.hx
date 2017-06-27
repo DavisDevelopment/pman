67328efa7d3558e0cf4cec12636416328f490522
DavisDevelopment <epikit@gmail.com>
2017-06-27 16:53:23 -0500

    pman.async.Task1 and pman.async.Task2 classes now implement Trackable

--- src/pman/async/Task1.hx
+++ src/pman/async/Task1.hx
@@ -1,23 +1,78 @@
 package pman.async;
 
-import haxe.Constraints.Function;
+import tannus.math.Percent;
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.async.Task1 as T1;
 
-class Task1 {
-    public function new() {
+import pman.Globals.*;
+import pman.async.Result;
 
+class Task1 extends T1 implements Trackable<Dynamic> {
+    /* Constructor Function */
+    public function new():Void {
+        super();
+
+        startTime = 0.0;
+        onComplete = new Signal();
+        onError = new Signal();
+        onResult = new Signal();
+        onComplete.on(function(res : Result<Dynamic,Dynamic>) {
+            switch ( res ) {
+                case Error( error ):
+                    onError.call( error );
+                case Value( result ):
+                    onResult.call( result );
+            }
+        });
+        onProgress = new Signal();
+        progress = 0.0;
     }
 
-    public function run(?cb : VoidCb):Void {
-        if (cb == null) {
-            cb = (function(?e) {
-                if (e != null)
-                    throw e;
+/* === Instance Methods === */
+
+    /**
+      * 
+      */
+    override function run(?done : VoidCb):Void {
+        var _cb = done;
+        done = function(?error : Dynamic) {
+            if (error != null)
+                onComplete.call(Result.Error( error ));
+            else
+                onComplete.call(Result.Value(null));
+        };
+        if (_cb != null) {
+            onComplete.once(function(res : Result<Dynamic,Dynamic>) {
+                switch ( res ) {
+                    case Error(e):
+                        _cb(e);
+                    case Value(v):
+                        _cb(null);
+                }
             });
         }
-        execute( cb );
+        startTime = now();
+        execute( done );
     }
 
-    private function execute(callback : VoidCb):Void {
-        callback('Error: not implemented');
+/* === Computed Instance Fields === */
+
+    public var progress(default, set):Float;
+    private function set_progress(v : Float):Float {
+        var _v = progress;
+        progress = v;
+        if (_v != progress) {
+            onProgress.call(new Delta(new Percent(progress), new Percent(_v)));
+        }
+        return progress;
     }
+
+/* === Instance Fields === */
+
+    public var startTime(default, null):Float;
+    public var onComplete : Signal<Result<Dynamic, Dynamic>>;
+    public var onError : Signal<Dynamic>;
+    public var onResult : Signal<Dynamic>;
+    public var onProgress : Signal<Delta<Percent>>;
 }
--- src/pman/async/Task2.hx
+++ src/pman/async/Task2.hx
@@ -1,25 +1,75 @@
 package pman.async;
 
-import haxe.Constraints.Function;
+import tannus.math.Percent;
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.async.Task2 as T2;
 
-class Task2<T> {
+import pman.Globals.*;
+import pman.async.Result;
+
+class Task2<T> extends T2<T> implements Trackable<T> {
+    /* Constructor Function */
     public function new() {
+        super();
 
+        startTime = 0.0;
+        onComplete = new Signal();
+        onError = new Signal();
+        onResult = new Signal();
+        onComplete.on(function(res : Result<Dynamic,T>) {
+            switch ( res ) {
+                case Error( error ):
+                    onError.call( error );
+                case Value( result ):
+                    onResult.call( result );
+            }
+        });
+        onProgress = new Signal();
+        progress = 0.0;
     }
 
-    public function run(?cb : Cb<T>):Void {
-        if (cb == null) {
-            cb = (function(?e, ?v:T) {
-                if (e != null)
-                    throw e;
-                else
-                    trace('result: $v');
+/* === Instance Methods === */
+
+    override function run(?cb : Cb<T>):Void {
+        var _cb = cb;
+        cb = function(?error:Dynamic, ?value:T) {
+            if (error != null)
+                onComplete.call(Result.Error( error ));
+            else
+                onComplete.call(Result.Value( value ));
+        };
+        if (_cb != null) {
+            onComplete.once(function(res : Result<Dynamic,T>) {
+                switch ( res ) {
+                    case Error(error):
+                        _cb(error, null);
+                    case Value(value):
+                        _cb(null, value);
+                }
             });
         }
+        startTime = now();
         execute( cb );
     }
 
-    private function execute(callback : Cb<T>):Void {
-        callback('Error: not implemented');
+/* === Computed Instance Fields === */
+
+    public var progress(default, set):Float;
+    private function set_progress(v : Float):Float {
+        var _v = progress;
+        progress = v;
+        if (_v != progress) {
+            onProgress.call(new Delta(new Percent(progress), new Percent(_v)));
+        }
+        return progress;
     }
+
+/* === Instance Fields === */
+
+    public var startTime(default, null):Float;
+    public var onComplete : Signal<Result<Dynamic,T>>;
+    public var onError : Signal<Dynamic>;
+    public var onResult : Signal<T>;
+    public var onProgress : Signal<Delta<Percent>>;
 }
