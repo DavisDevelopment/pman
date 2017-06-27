00b947ec0b90edd74c6e2da7aa7f7e4eb38a73b4
DavisDevelopment <epikit@gmail.com>
2017-06-27 16:49:54 -0500

    pman.async.VoidAsync is now a typedef to tannus.async.VoidAsync

--- src/pman/async/VoidAsync.hx
+++ src/pman/async/VoidAsync.hx
@@ -1,7 +1,11 @@
 package pman.async;
 
+typedef VoidAsync = tannus.async.VoidAsync;
+
+/*
 @:callable
 abstract VoidAsync (VoidCb->Void) from VoidCb->Void {
     public inline function new(f : VoidCb->Void)
         this = f;
 }
+*/
