31dbeb11acff5dad67020f9139c5ddd106fcbef0
DavisDevelopment <epikit@gmail.com>
2017-07-09 02:10:11 -0500

    created new pman.core.Engine class

---
+++ src/pman/core/Engine.hx
@@ -0,0 +1,23 @@
+package pman.core;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.Path;
+import tannus.sys.FileSystem as Fs;
+
+using StringTools;
+using tannus.ds.StringTools;
+using Slambda;
+using tannus.ds.ArrayTools;
+
+class Engine {
+    /* Constructor Function */
+    public function new():Void {
+        //
+    }
+
+/* === Instance Methods === */
+
+/* === Instance Fields === */
+
+}
