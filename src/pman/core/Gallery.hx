4570f18e180fa6f1e674d1aeb02c99ecd194c911
DavisDevelopment <epikit@gmail.com>
2017-07-09 03:52:54 -0500

    created new pman.core.Gallery class

---
+++ src/pman/core/Gallery.hx
@@ -0,0 +1,22 @@
+package pman.core;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.Path;
+
+using StringTools;
+using tannus.ds.StringTools;
+using Slambda;
+using tannus.ds.ArrayTools;
+
+class Gallery {
+    /* Constructor Function */
+    public function new():Void {
+        //
+    }
+
+/* === Instance Methods === */
+
+/* === Instance Fields === */
+
+}
