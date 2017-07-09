8cf1d08eb1b40defa56c5f9c2c5455f092090a98
DavisDevelopment <epikit@gmail.com>
2017-07-09 03:52:42 -0500

    created new pman.core.GalleryEntry class

---
+++ src/pman/core/GalleryEntry.hx
@@ -0,0 +1,24 @@
+package pman.core;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.Path;
+
+using StringTools;
+using tannus.ds.StringTools;
+using Slambda;
+using tannus.ds.ArrayTools;
+
+class GalleryEntry {
+    /* Constructor Function */
+    public function new():Void {
+        //
+    }
+
+/* === Instance Methods === */
+
+/* === Instance Fields === */
+
+    public var name : String;
+    public var entryType : GalleryEntryType;
+}
