9bd8b7eed3340114ce2b84a218d4b9b09d5aa5f1
DavisDevelopment <epikit@gmail.com>
2017-07-10 23:17:54 -0500

    pman.media.info.Dimensions now implements tannus.ds.IComparable

--- src/pman/media/info/Dimensions.hx
+++ src/pman/media/info/Dimensions.hx
@@ -1,9 +1,11 @@
 package pman.media.info;
 
-@:structInit
-class Dimensions {
-    public var w : Int;
-    public var h : Int;
+import tannus.ds.*;
+import tannus.geom2.*;
+
+import Reflect.compare as c;
+
+class Dimensions implements tannus.ds.IComparable<Dimensions> {
     public inline function new(w:Int, h:Int) {
         this.w = w;
         this.h = h;
@@ -13,7 +15,22 @@ class Dimensions {
         return (w == o.w && h == o.h);
     }
 
+    public function compareTo(o : Dimensions):Int {
+        var x = c(w, o.w);
+        if (x != 0)
+            return x;
+        else {
+            x = c(h, o.h);
+            return x;
+        }
+    }
+
+    public function toArea():Area<Int> return new Area(w, h);
+
     public function toString():String {
         return '${w}x${h}';
     }
+
+    public var w : Int;
+    public var h : Int;
 }
