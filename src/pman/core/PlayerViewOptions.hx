01f8bc7aa0f259207fb00ea34e713c3294d0df6b
DavisDevelopment <epikit@gmail.com>
2017-07-14 16:41:26 -0500

    created new pman.core.PlayerViewOptions class

---
+++ src/pman/core/PlayerViewOptions.hx
@@ -0,0 +1,26 @@
+package pman.core;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.Path;
+import tannus.graphics.Color;
+
+import pman.display.VideoFilter;
+
+using tannus.math.TMath;
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using tannus.ds.IteratorTools;
+
+class PlayerViewOptions extends EventDispatcher {
+    /* Constructor Function */
+    public function new():Void {
+        super();
+        __checkEvents = false;
+    }
+
+/* === Instance Fields === */
+}
