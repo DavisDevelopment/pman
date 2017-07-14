0ff4e432a97ee1ecabb3308aadc58377366c045c
DavisDevelopment <epikit@gmail.com>
2017-07-14 16:43:33 -0500

    filled out the pman.search.SearchTermParser enum

--- src/pman/search/SearchTerm.hx
+++ src/pman/search/SearchTerm.hx
@@ -3,6 +3,8 @@ package pman.search;
 import tannus.io.*;
 import tannus.ds.*;
 import tannus.sys.*;
+import tannus.nore.ORegEx;
+import tannus.sys.GlobStar;
 
 import pman.core.*;
 import pman.media.*;
@@ -23,5 +25,50 @@ using pman.media.MediaTools;
 using tannus.math.TMath;
 
 enum SearchTerm {
+    Constant(c : SearchTermConst);
     Word(word : String);
+    Phrase(text : String);
+    Parent(term : SearchTerm);
+    Call(name:String, params:Array<SearchTerm>);
+
+    Not(st : SearchTerm);
+    And(left:SearchTerm, right:SearchTerm);
+    Or(left:SearchTerm, right:SearchTerm);
+}
+
+enum SearchTermToken {
+    Word(w : String);
+    String(s : String);
+    FMacroParam(s : String);
+    OParent;
+    CParent;
+    OBracket;
+    CBracket;
+    OBox;
+    CBox;
+    FSlash;
+    BSlash;
+    Whitespace;
+    And;
+    Or;
+    Not;
+    Eq;
+    Lt;
+    Gt;
+    Dollar;
+    Circum;
+    Tilde;
+    Asterisk;
+    Colon;
+    Comma;
+}
+
+enum SearchTermConst {
+    CString(s : String);
+    CInt(i : Int);
+    CFloat(n : Float);
+    //CRaw
+    CEReg(re : EReg);
+    COReg(ore : String);
+    CGlob(glob : GlobStar);
 }
