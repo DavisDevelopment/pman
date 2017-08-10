df944d9fdbf446a92a643d5502cf5c3aed354955
DavisDevelopment <epikit@gmail.com>
2017-08-10 04:43:34 -0500

    created new vex.svg.SVGFilter class

---
+++ src/vex/vex/svg/SVGFilter.hx
@@ -0,0 +1,31 @@
+package vex.svg;
+
+import tannus.geom.*;
+
+import vex.svg.SVGElement.createElement;
+import vex.svg.path.Command;
+import vex.svg.path.*;
+
+import Std.*;
+import Math.*;
+import tannus.math.TMath.*;
+
+using Lambda;
+using tannus.ds.ArrayTools;
+using StringTools;
+using tannus.ds.StringUtils;
+using tannus.math.TMath;
+using tannus.macro.MacroTools;
+
+class SVGFilter extends SVGElement {
+	/* Constructor Function */
+	public function new():Void {
+		super();
+
+		e = createElement( 'filter' );
+	}
+
+/* === Computed Instance Fields === */
+
+
+}
