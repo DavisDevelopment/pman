f1035a4ac17fb238dad23a06db6faa7f850067fe
DavisDevelopment <epikit@gmail.com>
2017-09-12 00:47:59 -0500

    created new pman.events.PlayerDragDropEvent class

---
+++ src/pman/events/PlayerDragDropEvent.hx
@@ -0,0 +1,39 @@
+package pman.events;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.geom.*;
+import tannus.events.*;
+import tannus.html.fs.WebFile;
+
+import js.html.DragEvent as NativeDragEvent;
+import js.jquery.Event as JqEvent;
+
+import pman.ds.*;
+import pman.media.Track;
+import pman.Globals.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using tannus.ds.ArrayTools;
+using Lambda;
+using Slambda;
+
+@:allow( pman.ui.DragDropManager )
+class PlayerDragDropEvent extends DragDropEvent {
+    public function new(event:DragDropEvent, ?tracks:Array<Track>):Void {
+        super( event.e );
+
+        this.tracks = tracks;
+
+        //onDefaultPrevented.clear();
+        //onPropogationStopped.clear();
+        //onCancelled.clear();
+    }
+
+    public function playerPanePosition():Point {
+        return player.view.stage.globalToLocal(globalPosition());
+    }
+
+    public var tracks : Maybe<Array<Track>>;
+}
