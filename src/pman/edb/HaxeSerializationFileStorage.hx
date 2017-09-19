6d342334af7e806df283b062ee54a120f661586b
DavisDevelopment <epikit@gmail.com>
2017-09-19 12:26:23 -0500

    created new pman.edb.HaxeSerializationFileStorage class

---
+++ src/pman/edb/HaxeSerializationFileStorage.hx
@@ -0,0 +1,33 @@
+package pman.edb;
+
+import tannus.ds.*;
+import tannus.io.*;
+import tannus.sys.*;
+import tannus.sys.FileSystem as Fs;
+
+import haxe.Constraints.Function;
+import haxe.Serializer;
+import haxe.Unserializer;
+import haxe.Json;
+
+import pman.edb.Modem;
+import pman.edb.Port;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+
+class HaxeSerializationFileStorage extends Storage {
+    public function new(path : Path):Void {
+        super();
+
+        this.path = path;
+
+        this.modem = new HxSerializationModem();
+        this.port = new TextFilePort( path );
+    }
+
+    public var path : Path;
+}
