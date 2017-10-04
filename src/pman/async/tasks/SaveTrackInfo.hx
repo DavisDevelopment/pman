52536ceb7fd620603f3f91f963ad441518aa9dfa
DavisDevelopment <epikit@gmail.com>
2017-10-04 18:55:51 -0500

    created new pman.async.tasks.SaveTrackInfo class

---
+++ src/pman/async/tasks/SaveTrackInfo.hx
@@ -0,0 +1,126 @@
+package pman.async.tasks;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+
+import electron.Shell;
+
+import pman.core.*;
+import pman.media.*;
+import pman.edb.*;
+import pman.edb.MediaStore;
+import pman.async.*;
+import pman.media.info.*;
+import pman.Globals.*;
+
+import Std.*;
+import tannus.math.TMath.*;
+import electron.Tools.defer;
+import Slambda.fn;
+
+using tannus.math.TMath;
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using pman.media.MediaTools;
+using pman.async.Asyncs;
+using pman.async.VoidAsyncs;
+
+class SaveTrackInfo extends Task1 {
+    /* Constructor Function */
+    public function new(track:Track, delta:TrackInfoFormValueDelta):Void {
+        super();
+
+        this.track = track;
+        this.delta = delta;
+    }
+
+/* === Instance Methods === */
+
+    /**
+      * execute [this] Task
+      */
+    override function execute(done : VoidCb):Void {
+        [rename_track, edit_data].series( done );
+    }
+
+    /**
+      * rename the Track, if necessary
+      */
+    private function rename_track(done : VoidCb):Void {
+        if (delta.title != null) {
+            var newPath:Path = track.getFsPath();
+            newPath.name = delta.title.current;
+            newPath = newPath.normalize();
+            var renamer = new TrackRename(track, database.mediaStore, newPath);
+            renamer.run( done );
+        }
+        else {
+            done();
+        }
+    }
+
+    /**
+      * do the stuff
+      */
+    private function edit_data(done : VoidCb):Void {
+        track.editData(function(data, next) {
+            // create list to hold sub-tasks
+            var steps:Array<VoidAsync> = new Array();
+
+            // handle synchronous changes
+            steps.push(function(end) {
+                if (delta.rating != null)
+                    data.rating = delta.rating.current;
+
+                if (delta.tags != null)
+                    data.tags = delta.tags.current;
+
+                if (delta.description != null)
+                    data.description = delta.description.current;
+
+                end();
+            });
+
+            // handle Actors
+            steps.push(function(end) {
+                if (delta.actors != null) {
+                    if (delta.actors.current != null) {
+                        var newActors = delta.actors.current;
+                        data.writeActors(newActors, function(?error, ?al) {
+                            end( error );
+                        });
+                    }
+                    else {
+                        end();
+                    }
+                }
+                else {
+                    end();
+                }
+            });
+
+            // run those sub-tasks
+            steps.series( next );
+
+        }, function(?error) {
+            done( error );
+        });
+    }
+
+/* === Instance Fields === */
+
+    public var track : Track;
+    public var delta : TrackInfoFormValueDelta;
+}
+
+typedef TrackInfoFormValueDelta = {
+    ?title: Delta<String>,
+    ?description: Delta<String>,
+    ?tags: Delta<Array<String>>,
+    ?actors: Delta<Array<String>>,
+    ?rating: Delta<Float>
+};
