49dac37a271d9ca6c916dc19f21f9a8257811cb0
DavisDevelopment <epikit@gmail.com>
2017-10-04 05:04:54 -0500

    added 'dialogs' field to pman.core.Engine

--- src/pman/core/Engine.hx
+++ src/pman/core/Engine.hx
@@ -5,6 +5,8 @@ import tannus.ds.*;
 import tannus.sys.Path;
 import tannus.sys.FileSystem as Fs;
 
+import pman.core.engine.*;
+
 using StringTools;
 using tannus.ds.StringUtils;
 using Slambda;
@@ -14,6 +16,7 @@ class Engine {
     /* Constructor Function */
     public function new():Void {
         executor = new Executor();
+        dialogs = new Dialogs( this );
     }
 
 /* === Instance Methods === */
@@ -22,4 +25,6 @@ class Engine {
 
     // a micro-task manager and executor
     public var executor : Executor;
+
+    public var dialogs : Dialogs;
 }
