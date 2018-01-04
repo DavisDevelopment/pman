01740adb1f1a6e04c01ef0f5df6cf7f1cab5cf97
DavisDevelopment <epikit@gmail.com>
2018-01-04 10:43:14 -0600

    created new pman.bg.db.Database class

---
+++ src/pman/bg/db/Database.hx
@@ -0,0 +1,111 @@
+package pman.bg.db;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+import tannus.async.*;
+import tannus.sys.Path;
+import tannus.http.Url;
+import tannus.sys.FileSystem as Fs;
+
+import edis.libs.nedb.*;
+import edis.storage.db.*;
+import edis.core.Prerequisites;
+
+import Slambda.fn;
+import edis.Globals.*;
+
+import pman.bg.Dirs;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Slambda;
+using tannus.ds.ArrayTools;
+using tannus.FunctionTools;
+using pman.bg.MediaTools;
+
+class Database {
+    /* Constructor Function */
+    public function new():Void {
+        tables = new Dict();
+        reqs = new Prerequisites();
+
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+/* === Instance Methods === */
+
+    /**
+      * initialize [this] Database instance
+      */
+    public function init(done: VoidCb):Void {
+        _build();
+
+        reqs.meet( done );
+    }
+
+    /**
+      * get a new Table
+      */
+    public function openTable<T:Table>(name:String, ?tableClass:Class<T>):T {
+        if (tableClass == null) {
+            tableClass = untyped Table;
+        }
+
+        if (!name.endsWith('.db')) {
+            name += '.db';
+        }
+
+        if (!tables.exists( name )) {
+            var table:T = Type.createInstance(tableClass, untyped [getStore(name)]);
+            tables.set(name, cast table);
+            reqs.vasync( table.init );
+            return table;
+        }
+        else {
+            return cast tables.get( name );
+        }
+    }
+
+    /**
+      * open up a new DataStore
+      */
+    private function getStore(name: String):DataStore {
+        if (!name.endsWith('.db')) {
+            name += '.db';
+        }
+        if (_stores.exists( name )) {
+            return _stores[name];
+        }
+        else {
+            return (_stores[name] = new DataStore({
+                filename: Dirs.dbPath( name ).toString()
+            }));
+        }
+    }
+
+    /**
+      * build [this] Database instance
+      */
+    private inline function _build():Void {
+        actors = openTable('actors', ActorTable);
+        tags = openTable('tags', TagTable);
+        media = openTable('media', MediaTable);
+    }
+
+/* === Instance Fields === */
+
+    public var media: MediaTable;
+    public var actors: ActorTable;
+    public var tags: TagTable;
+
+    public var tables: Dict<String, Table>;
+    public var reqs: Prerequisites;
+
+/* === Static Fields === */
+
+    private static var _stores:Dict<String, DataStore> = {new Dict();};
+    public static var instance:Null<Database> = null;
+}
