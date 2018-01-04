331a0e6fb06b54581802c20f4ffbda1b5610e0a0
DavisDevelopment <epikit@gmail.com>
2018-01-04 05:35:16 -0600

    created new pman.bg.media.MediaSource enum

---
+++ src/pman/bg/media/MediaSource.hx
@@ -0,0 +1,22 @@
+package pman.bg.media;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+import tannus.async.*;
+import tannus.sys.Path;
+import tannus.http.Url;
+
+import Slambda.fn;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Slambda;
+using tannus.ds.ArrayTools;
+using tannus.FunctionTools;
+using pman.bg.URITools;
+
+enum MediaSource {
+    MSLocalPath(path: Path);
+    MSUrl(url: String);
+}
