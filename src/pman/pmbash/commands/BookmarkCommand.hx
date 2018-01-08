3cf2bf5f40d964e07c96cb4a08f8d1bd3e891085
DavisDevelopment <epikit@gmail.com>
2018-01-07 18:09:47 -0600

    created new pman.pmbash.commands.BookmarkCommand

---
+++ src/pman/pmbash/commands/BookmarkCommand.hx
@@ -0,0 +1,41 @@
+package pman.pmbash.commands;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.async.*;
+import tannus.math.*;
+import tannus.TSys as Sys;
+
+import pman.core.*;
+import pman.async.*;
+import pman.format.pmsh.*;
+import pman.format.pmsh.Token;
+import pman.format.pmsh.Expr;
+import pman.format.pmsh.Cmd;
+import pman.pmbash.commands.*;
+
+import Slambda.fn;
+import tannus.math.TMath.*;
+import edis.Globals.*;
+import pman.Globals.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using tannus.FunctionTools;
+using tannus.async.Asyncs;
+
+class BookmarkCommand extends Command {
+    /**
+      * execute [this] command
+      */
+    override function execute(i:Interpreter, argv:Array<CmdArg>, done:VoidCb):Void {
+        var args:Array<Dynamic> = argv.map.fn( _.value );
+        var action:Null<String> = args.shift();
+        if (action == null) {
+            player.addBookmark( done );
+        }
+    }
+}
