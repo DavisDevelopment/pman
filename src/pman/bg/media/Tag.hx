af4b0ba99039751485c2bc01f578f752e2a38c40
DavisDevelopment <epikit@gmail.com>
2018-01-08 22:03:05 -0600

    created new pman.bg.media.Tag class

---
+++ src/pman/bg/media/Tag.hx
@@ -0,0 +1,29 @@
+package pman.bg.media;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+import tannus.async.*;
+import tannus.async.promises.*;
+import tannus.sys.Path;
+import tannus.http.Url;
+import tannus.sys.FileSystem as Fs;
+
+import Slambda.fn;
+import edis.Globals.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Slambda;
+using tannus.ds.ArrayTools;
+using tannus.FunctionTools;
+using tannus.async.Asyncs;
+using pman.bg.MediaTools;
+
+class Tag {
+    public inline function new(row: TagRow) {
+
+    }
+
+    public var name: String;
+}
