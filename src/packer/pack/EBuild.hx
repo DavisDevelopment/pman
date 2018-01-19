f2608ed6ed005e06cbe69d5f3bfa652d9facb031
DavisDevelopment <epikit@gmail.com>
2018-01-18 23:06:58 -0600

    created new packer.pack.EBuild

---
+++ src/packer/pack/EBuild.hx
@@ -0,0 +1,23 @@
+package pack;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.*;
+import tannus.async.*;
+
+import haxe.Json;
+import js.Lib.require;
+
+import pack.*;
+import pack.Tools.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using pack.Tools;
+
+class EBuild extends Task {
+    //
+}
