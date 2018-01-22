533d26c3c716da83a8ad4327ced4d625cffd9f5a
DavisDevelopment <epikit@gmail.com>
2018-01-22 01:03:18 -0600

    the FileFilter class can now include Mime type information

--- src/electron/ext/FileFilter.hx
+++ src/electron/ext/FileFilter.hx
@@ -1,25 +1,67 @@
 package electron.ext;
 
+import tannus.sys.GlobStar;
+
 using StringTools;
 using tannus.ds.StringUtils;
-using Lambda;
+using Slambda;
+using tannus.ds.ArrayTools;
 
 @:structInit
 class FileFilter {
 	/* Constructor Function */
-	public function new(name:String, extensions:Array<String>):Void {
+	public function new(name:String, extensions:Array<String>, ?mimes:Array<String>):Void {
 		this.name = name;
 		this.extensions = extensions;
+		this.mimes = mimes;
 	}
 
 /* === Instance Methods === */
 
+    /**
+      * combine [this] with [other]
+      */
     public function plus(other:FileFilter, ?sumname:String):FileFilter {
         if (sumname == null)
             sumname = (name + other.name);
-        return new FileFilter(sumname, extensions.concat(other.extensions));
+        var summimes = null;
+        if (mimes == null && other.mimes == null) {
+            summimes = null;
+        }
+        else {
+            summimes = [];
+            if (mimes != null)
+                summimes = summimes.concat( mimes );
+
+            if (other.mimes != null)
+                summimes = summimes.concat( other.mimes );
+            summimes = summimes.compact().unique();
+        }
+        return new FileFilter(sumname, extensions.concat(other.extensions), summimes);
     }
 
+    /**
+      * perform checks on mime type
+      */
+    public function testMime(mime: String):Bool {
+        if (mimes == null) {
+            return false;
+        }
+        else {
+            var glob:GlobStar;
+            for (m in mimes) {
+                glob = new GlobStar(m, 'i');
+                if (glob.test( mime )) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    /**
+      * perform basic extension-name check
+      */
     public inline function test(path : String):Bool {
         return extensions.has(path.afterLast( '.' ));
     }
@@ -28,6 +70,7 @@ class FileFilter {
 
 	public var name : String;
 	public var extensions : Array<String>;
+	public var mimes : Null<Array<String>>;
 
 /* === Static Fields === */
 
@@ -40,16 +83,20 @@ class FileFilter {
 	public static function __init__():Void {
 		VIDEO = new FileFilter('Video Files', [
 			'mp4', 'webm', 'ogv'
-		]);
+		], ['video/*']);
+
 		AUDIO = new FileFilter('Audio Files', [
-			'mp3', 'ogg', 'wav'
-		]);
+			'mp3', 'ogg', 'wav', 'webm'
+		], ['audio/*']);
+
 		PLAYLIST = new FileFilter('Playlist Files', [
 			'm3u', 'xspf', 'pls', 'zip'
 		]);
+
 		IMAGE = new FileFilter('Image Files', [
-		    'png', 'jpg', 'jpeg'
-		]);
-		ALL = VIDEO.plus(AUDIO).plus(PLAYLIST, 'All Files');
+		    'png', 'jpg', 'jpeg', 'svg', 'bmp', 'webp'
+		], ['image/*']);
+
+		ALL = VIDEO.plus(AUDIO).plus(IMAGE).plus(PLAYLIST, 'All Files');
 	}
 }
