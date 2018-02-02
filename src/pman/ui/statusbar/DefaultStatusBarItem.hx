af757ffac7c386fa5b70aaaa0994c0f2b049ac2d
DavisDevelopment <epikit@gmail.com>
2018-02-01 20:07:53 -0600

    mostly rewrote pman.ui.statusbar.DefaultStatusBarItem in preparation for making it a much more useful (less pointless) part of the interface

--- src/pman/ui/statusbar/DefaultStatusBarItem.hx
+++ src/pman/ui/statusbar/DefaultStatusBarItem.hx
@@ -36,7 +36,7 @@ class DefaultStatusBarItem extends StatusBarItem {
         tb = new TextBox();
         tb.fontFamily = 'Ubuntu';
         tb.fontSizeUnit = 'px';
-        tb.fontSize = 15;
+        tb.fontSize = 10;
         tb.color = new Color(255, 255, 255);
     }
 
@@ -59,30 +59,82 @@ class DefaultStatusBarItem extends StatusBarItem {
     override function update(stage : Stage):Void {
         super.update( stage );
 
-        var oldText = tb.text;
-        if (player.track == null) {
-            tb.text = '';
-        }
-        else {
-            switch ( player.track.source ) {
-                case MediaSource.MSLocalPath( path ):
-                    tb.text = path;
-                case MediaSource.MSUrl( url ):
-                    tb.text = url;
+        // reset some of the content state to default
+        resetContent();
+
+        // shorthand reference to [player.track]
+        var t:Null<Track> = player.track;
+
+        // whether [this]'s displayed content is textual
+        var isText:Bool = true;
+
+        //TODO determine what type of content is to be displayed...
+
+        // if content is textual
+        if ( isText ) {
+            // the 'old' textual content of the text box
+            var oldText:String = tb.text;
+
+            // the 'old' area of the text box
+            var oa:Area<Float> = new Area(tb.width, tb.height);
+
+            // if there is no track, render blank string
+            if (t == null) {
+                tb.text = '';
+            }
+            else {
+                // based on MediaSource type, display either...
+                switch ( t.source ) {
+                    // a FileSystem Path for local media
+                    case MediaSource.MSLocalPath( path ):
+                        tb.text = path;
+
+                    // or a URI for remote media
+                    case MediaSource.MSUrl( url ):
+                        tb.text = url;
+                }
+
+                // if [t] has its metadata loaded, and it's favorited
+                if (t.data != null && t.data.starred) {
+                    // prepend a denotation to the text
+                    tb.text = (' (favorited) ' + tb.text);
+                }
             }
 
-            var t = player.track;
-            if (t.data != null && t.data.starred) {
-                tb.text = (' (favorited) ' + tb.text);
+            // the 'new' Area of the text box
+            var na:Area<Float> = new Area(tb.width, tb.height);
+
+            // if the text's dimensions have changed
+            if (na.nequals( oa )) {
+                // set content rect
+                ch = na.height;
+                cw = max(w, na.width);
             }
         }
+    }
 
-        if (oldText != tb.text) {
-            tb.autoScale(null, rect.h);
-        }
+    /**
+      * calculate [this]'s geometry
+      */
+    override function calculateGeometry(r: Rect<Float>):Void {
+        super.calculateGeometry( r );
+    }
+
+    /**
+      *
+      */
+    private function resetContent():Void {
+        ch = 0.0;
+        cw = 0.0;
+
+        atb = new Array();
     }
 
 /* === Instance Fields === */
 
+    // single TextBox for rendering simple text information
     private var tb : TextBox;
+
+    // array of TextBoxes, for rendering dynamic hunks of multi-styling text
+    private var atb: Array<TextBox>;
 }
