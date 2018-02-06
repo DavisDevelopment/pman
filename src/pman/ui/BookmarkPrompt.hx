542a9a0a45545b97ee166af37e22bb78f3bcad87
DavisDevelopment <epikit@gmail.com>
2018-02-05 19:39:03 -0600

    added 'select' input to the BookmarkPrompt widget for selecting mark type

--- src/pman/ui/BookmarkPrompt.hx
+++ src/pman/ui/BookmarkPrompt.hx
@@ -26,16 +26,89 @@ class BookmarkPrompt extends PromptBox {
     public function new():Void {
         super();
 
-        title = 'bookmark name';
+        addClass('pman-mark-prompt');
+
+        typeSelect = new Select();
+        inline function o(n:String, v:PMarkType) return typeSelect.option(n, v);
+
+        o('normal', PMTNamed);
+        o('scene', PMTScene);
+        o('start time', PMTStartTime);
+        o('end time', PMTEndTime);
+
+        inputRow.append( typeSelect );
+        typeSelect.onchange.on(_changedMarkType);
+
+        title = 'Create Bookmark';
+        placeholder = 'bookmark title';
     }
 
 /* === Instance Methods === */
+
+    /**
+      * use [this] Prompt to read a Mark instance
+      */
+    public function readMark(callback: Null<Mark>->Void):Void {
+        open();
+        readLine(function(line: String) {
+            if (mtype == null)
+                mtype = PMTNamed;
+
+            var time:Float = player.currentTime;
+            var type:MarkType;
+            switch ( mtype ) {
+                case PMTNamed:
+                    type = MarkType.Named( line );
+                case PMTStartTime:
+                    type = MarkType.Begin;
+                case PMTEndTime:
+                    type = MarkType.End;
+                case PMTScene:
+                    var t = player.track;
+                    var st:SceneMarkType = SceneMarkType.SceneBegin;
+                    if (t != null && t.data != null && t.data.marks != null) {
+                        for (m in t.data.marks) {
+                            if (m.type.match(Scene(SceneBegin, line))) {
+                                st = SceneEnd;
+                                break;
+                            }
+                        }
+                    }
+                    type = MarkType.Scene(st, line);
+                default:
+                    type = MarkType.Named(line);
+            }
+
+            var mark:Mark = new Mark(type, time);
+            callback( mark );
+        });
+        focus();
+    }
+
+    override function __listen():Void {
+        super.__listen();
+
+        //typeSelect.forwardEvent('keydown', null, KeyboardEvent.fromJqEvent);
+        //typeSelect.on('keydown', function(event: KeyboardEvent))
+    }
 
     /**
       * handle keys
       */
     override function keydown(event : KeyboardEvent):Void {
         switch ( event.key ) {
+            case Enter:
+                if (mtype != null && mtype.match(PMTStartTime|PMTEndTime)) {
+                    line('');
+                }
+                else if (value.hasContent()) {
+                    line(value.trim());
+                }
+                else {
+                    empty();
+                }
+                close();
+                //
             case Up:
                 event.cancel();
                 if (peekDistance == -1) {
@@ -91,8 +164,38 @@ class BookmarkPrompt extends PromptBox {
         history.unshift(line.trim());
     }
 
+    private function _changedMarkType(d: Delta<PMarkType>):Void {
+        var type:PMarkType = d.current;
+        if (type == null) type = PMarkType.PMTNamed;
+
+        switch ( type ) {
+            case PMTStartTime|PMTEndTime:
+                value = '';
+                placeholder = 'the selected mark type doesn\'t accept a title';
+                //input.el.set('disabled', 'yes');
+
+            case nameable:
+                //input.el.removeAttr('disabled');
+                placeholder = 'bookmark title';
+        }
+
+        defer(function() {
+            focus();
+        });
+    }
+
+/* === Computed Instance Fields === */
+
+    public var mtype(get, set): Null<PMarkType>;
+    private inline function get_mtype() return typeSelect.getValue();
+    private function set_mtype(v) {
+        typeSelect.setValue( v );
+        return mtype;
+    }
+
 /* === Instance Fields === */
 
+    public var typeSelect : Select<PMarkType>;
 
     private var peekDistance : Int = -1;
     private var originalValue : String;
@@ -100,4 +203,12 @@ class BookmarkPrompt extends PromptBox {
 /* === Statics === */
 
     public static var history : Array<String> = {new Array();};
+}
+
+/* enum of 'mark type's */
+enum PMarkType {
+    PMTNamed;
+    PMTScene;
+    PMTStartTime;
+    PMTEndTime;
 }
