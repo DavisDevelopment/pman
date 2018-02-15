2a0e3c0bab5e31b42837c68b778bdcae7e1b6ef3
DavisDevelopment <epikit@gmail.com>
2018-02-14 22:11:45 -0600

    added I/O-related fields to pman.format.pmsh.Cmd

--- src/pman/format/pmsh/Cmd.hx
+++ src/pman/format/pmsh/Cmd.hx
@@ -2,6 +2,13 @@ package pman.format.pmsh;
 
 import tannus.io.*;
 import tannus.ds.*;
+import tannus.node.*;
+import tannus.node.WritableStream;
+import tannus.node.ReadableStream;
+import tannus.node.Duplex;
+import tannus.node.Transform;
+
+import edis.streams.*;
 
 import pman.async.*;
 import pman.format.pmsh.Token;
@@ -34,6 +41,12 @@ class Cmd {
 /* === Instance Fields === */
 
     public var name : String;
+    public var stdout: WritableStream<ByteArray>;
+    public var stderr: WritableStream<ByteArray>;
+    public var stdin: ReadableStream<ByteArray>;
+
+    private var argv: Array<CmdArg>;
+    private var interpreter: Interpreter;
 }
 
 @:structInit
