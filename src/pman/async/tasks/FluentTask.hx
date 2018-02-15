4526e9e36901233d8ed03760867332b10a3a0293
DavisDevelopment <epikit@gmail.com>
2018-02-14 22:10:23 -0600

    started creating new pman.async.tasks.FluentTask class

---
+++ src/pman/async/tasks/FluentTask.hx
@@ -0,0 +1,36 @@
+package pman.async.tasks;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.sys.Path;
+import tannus.sys.FileSystem as Fs;
+import tannus.geom2.Area;
+import tannus.async.*;
+import tannus.node.Buffer;
+
+import ffmpeg.Fluent;
+
+import Std.*;
+import tannus.math.TMath.*;
+import Slambda.fn;
+import edis.Globals.*;
+import pman.Globals.*;
+
+using tannus.math.TMath;
+using StringTools;
+using tannus.ds.StringUtils;
+using Slambda;
+using tannus.ds.ArrayTools;
+using tannus.async.Asyncs;
+
+class FluentTask extends Task1 {
+    /* Constructor Function */
+    public function new():Void {
+        super();
+    }
+
+/* === Instance Methods === */
+
+/* === Instance Fields === */
+
+}
