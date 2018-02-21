1ee20b79641abade1f61addd943ef6b8f4eb712b
DavisDevelopment <epikit@gmail.com>
2018-02-20 22:52:20 -0600

    pman.core.Engine now has 'init' method

--- src/pman/core/Engine.hx
+++ src/pman/core/Engine.hx
@@ -4,9 +4,13 @@ import tannus.io.*;
 import tannus.ds.*;
 import tannus.sys.Path;
 import tannus.sys.FileSystem as Fs;
+import tannus.async.*;
 
 import pman.core.engine.*;
 import pman.edb.*;
+
+import edis.Globals.*;
+import pman.Globals.*;
 
 using StringTools;
 using tannus.ds.StringUtils;
@@ -20,9 +24,39 @@ class Engine {
         dialogs = new Dialogs( this );
         appDir = new AppDir();
         db = new PManDatabase();
+
+        rs = new OnceSignal();
     }
 
 /* === Instance Methods === */
+
+    /**
+      * initialize [this] Engine
+      */
+    public function init(?done: VoidCb):Void {
+        if (done == null) {
+            done = function(?error: Dynamic) {
+                if (error != null) {
+                    report( error );
+                }
+            };
+        }
+
+        db.init(function(?error) {
+            if (error != null) {
+                done( error );
+            }
+            else {
+                rs.announce();
+                done();
+            }
+        });
+    }
+
+    public inline function isReady():Bool { return rs.isReady(); }
+    public inline function onReady(f: Void->Void):Void return rs.on( f );
+
+/* === Computed Instance Fields === */
 
 /* === Instance Fields === */
 
@@ -37,4 +71,6 @@ class Engine {
 
     // utility object for working with files and paths related to the application's location on the host filesystem
     public var appDir : AppDir;
+
+    private var rs: OnceSignal;
 }
