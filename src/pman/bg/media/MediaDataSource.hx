88e7c6caa1c35676b32d779d9462a8061c097044
DavisDevelopment <epikit@gmail.com>
2018-03-19 22:45:21 -0500

    created new pman.bg.media.MediaDataSource module

---
+++ src/pman/bg/media/MediaDataSource.hx
@@ -0,0 +1,33 @@
+package pman.bg.media;
+
+import tannus.ds.Object;
+import tannus.ds.Dict;
+
+import pman.bg.media.MediaRow;
+
+enum MediaDataSource {
+    Empty;
+    Partial(properties:Array<String>, data:MediaDataSourceState);
+    Complete(row: MediaDataSourceState);
+}
+
+enum MediaDataSourceDecl {
+    Empty;
+    Partial(properties: Array<String>);
+    Complete;
+}
+
+typedef MediaDataSourceState = {
+    row: MediaRow,
+    initial: NullableMediaDataState,
+    current: NullableMediaDataState
+};
+
+typedef NullableMediaDataState = {
+    >NullableRawBaseMediaData,
+
+    ?marks: Array<Mark>,
+    ?tags: Array<Tag>,
+    ?actors: Array<Actor>,
+    ?attrs: Dict<String, Dynamic>
+};
