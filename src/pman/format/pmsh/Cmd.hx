6e28e0b871dc6970d03368ee49a543d03be1976c
DavisDevelopment <epikit@gmail.com>
2018-05-17 20:46:38 -0500

    began to update pmsh.Cmd class

--- src/pman/format/pmsh/Cmd.hx
+++ src/pman/format/pmsh/Cmd.hx
@@ -23,27 +23,123 @@ using tannus.ds.ArrayTools;
 using Slambda;
 using pman.async.VoidAsyncs;
 
+/**
+  class to represent a PmSh (PMan Shell) command
+ **/
 class Cmd {
     /* Constructor Function */
     public function new():Void {
-
+        name = 'cmd';
+        stdio = _createStdIo();
     }
 
 /* === Instance Methods === */
 
     /**
-      * execute [this] Cmd
-      */
+      execute [this] Cmd
+      TODO make this method final, and make a '_run' method
+     **/
     public function execute(interp:Interpreter, args:Array<CmdArg>, done:VoidCb):Void {
         done();
     }
 
+    private function _destroy(error:Null<Dynamic>, callback:VoidCb):Void {
+        callback( error );
+    }
+
+    private function _kill(code:Int, callback:VoidCb):Void {
+        //TODO actually kill the Cmd
+        _destroy(null, callback);
+    }
+
+    public function print(x: Dynamic):Void {
+        stdout.write(bytes(x));
+    }
+
+    public function println(x: Dynamic):Void {
+        print( x );
+        print('\n');
+    }
+
+    /**
+      convert [x] to a ByteArray and return it
+     **/
+    private function bytes(x: Dynamic):Null<ByteArray> {
+        if (x == null) {
+            return null;
+        }
+        else if ((x is Binary)) {
+            return cast x;
+        }
+        else if ((x is Buffer)) {
+            return cast tannus.io.impl.NodeBinary.ofData(cast x);
+        }
+        else if ((x is String)) {
+            return ByteArray.ofString( x );
+        }
+        else if ((x is haxe.io.Bytes)) {
+            return ByteArray.fromBytes(cast x);
+        }
+        else {
+            inline function has(name: String):Bool {
+                return Reflect.hasField(x, name);
+            }
+
+            try {
+                if (has('toBytes')) {
+                    return bytes(x.toBytes());
+                }
+                else if (has('toByteArray')) {
+                    return bytes(x.toByteArray());
+                }
+                else if (has('toBinary')) {
+                    return bytes(x.toBinary());
+                }
+                else {
+                    return ByteArray.ofString(Std.string( x ));
+                }
+            }
+            catch (error: Dynamic) {
+                return ByteArray.ofString(Std.string( x ));
+            }
+        }
+    }
+
+    private function _createStdOut():WritableStream<ByteArray> {
+        return new WritableStream({});
+    }
+
+    private function _createStdErr():WritableStream<ByteArray> {
+        return new WritableStream({});
+    }
+
+    private function _createStdIn():ReadableStream<ByteArray> {
+        return new ReadableStream({});
+    }
+
+    private function _createStdIo():CmdStdIo {
+        return {
+            input: _createStdIn(),
+            output: _createStdOut(),
+            error: _createStdErr()
+        };
+    }
+
+/* === Computed Instance Fields === */
+
+    public var stdout(get, never): WritableStream<ByteArray>;
+    private inline function get_stdout() return stdio.output;
+
+    public var stderr(get, never): WritableStream<ByteArray>;
+    private inline function get_stderr() return stdio.error;
+
+    public var stdin(get, never): ReadableStream<ByteArray>;
+    private inline function get_stdin() return stdio.input;
+
 /* === Instance Fields === */
 
     public var name : String;
-    public var stdout: WritableStream<ByteArray>;
-    public var stderr: WritableStream<ByteArray>;
-    public var stdin: ReadableStream<ByteArray>;
+    public var stdio : CmdStdIo;
 
     private var argv: Array<CmdArg>;
     private var interpreter: Interpreter;
@@ -58,4 +154,10 @@ class CmdArg {
         expr = e;
         value = v;
     }
+}
+
+typedef CmdStdIo = {
+    var error: WritableStream<ByteArray>;
+    var output: WritableStream<ByteArray>;
+    var input: ReadableStream<ByteArray>;
 }
