98cd78a3dd53c496faa2f96dd641082a926e1094
DavisDevelopment <epikit@gmail.com>
2018-05-31 15:17:58 -0500

    added a bunch of stream-/thumbnail-related stuff to Bundle class

--- src/pman/media/info/Bundle.hx
+++ src/pman/media/info/Bundle.hx
@@ -2,12 +2,15 @@ package pman.media.info;
 
 import tannus.io.*;
 import tannus.ds.*;
-//import tannus.ds.promises.*;
 import tannus.math.*;
 import tannus.geom.*;
 import tannus.sys.Path;
 import tannus.sys.FileSystem as Fs;
+import tannus.math.Time;
+import tannus.math.Random;
+import tannus.media.Duration;
 import tannus.async.*;
+import tannus.stream.StreamInput;
 import tannus.async.promises.*;
 
 import gryffin.display.Image;
@@ -15,25 +18,32 @@ import gryffin.display.Image;
 import pman.async.tasks.*;
 import pman.media.info.BundleItemType;
 import pman.bg.media.Dimensions;
+import pman.format.time.TimeExpr;
+import pman.format.time.TimeParser;
 
 import haxe.Json;
 import haxe.Serializer;
 import haxe.Unserializer;
+import haxe.extern.EitherType as Either;
 
 import tannus.math.TMath.*;
+import Slambda.fn;
 import edis.Globals.*;
 import pman.Globals.*;
-import Slambda.fn;
 
-using tannus.math.TMath;
-using Slambda;
-using tannus.ds.AnonTools;
+using tannus.ds.IteratorTools;
 using tannus.FunctionTools;
+using tannus.ds.AnonTools;
+using tannus.math.TMath;
 using StringTools;
 using tannus.ds.StringUtils;
+using Slambda;
 using tannus.ds.ArrayTools;
-using tannus.ds.IteratorTools;
+using tannus.async.Asyncs;
 
+/**
+  represents a folder in which asset files associated with [track]'s media are stored
+ **/
 class Bundle {
     /* Constructor Function */
     public function new(track : Track):Void {
@@ -43,8 +53,9 @@ class Bundle {
 
         _ic = new Map();
         tgee = new EventDispatcher();
-        @:privateAccess
+        @:privateAccess {
             tgee.__checkEvents = false;
+        }
     }
 
 /* === Instance Methods === */
@@ -163,6 +174,143 @@ class Bundle {
     }
 
     /**
+      create and return a readable-stream of BundleItem instances corresponding with the [times] provided
+     **/
+    public function streamMultipleSnapshots(times:Array<Float>, ?size:String):StreamInput<BundleItem> {
+        if (size == null) {
+            size = '10%';
+        }
+
+        // parse actual dimensions from [size]
+        var dim = sizeDimensions( size );
+        // create variable to hold BundleItem results
+        var items:Array<BundleItem> = new Array();
+
+        var iw:StreamInputPusher<BundleItem> = null;
+
+        inline function add(i: BundleItem):Bool {
+            return iw.next( i );
+        }
+
+        inline function flush() {
+            var keepReading: Bool = true;
+            if (iw != null && items.hasContent()) {
+                do {
+                    keepReading = iw.next(items.shift());
+                }
+                while (keepReading && !items.empty());
+            } 
+
+            return keepReading;
+        }
+
+        function onAllBuffered() {
+            null;
+        }
+
+        var _status = [
+            false, // $0 initialization complete
+            false, // $1 pre-existing items buffered
+            false, // $2 generator task started
+            false, // $3 newly-created items buffered
+            false, // $4 all items buffered
+            false
+        ];
+
+        inline function status(i:Int, ?v:Bool):Bool {
+            return (v != null ? _status[i] = v : _status[i]);
+        }
+
+
+        function _init(o, done:VoidCb) {
+            iw = o;
+
+            //status(0, true);
+            var missing = [];
+
+            for (time in times) {
+                var pess = findSnapshot(time, size);
+                if (pess != null) {
+                    add( pess );
+                }
+                else {
+                    missing.push( time );
+                }
+            }
+            status(1, true);
+
+            vsequence(
+                function(task, exec) {
+                    task(function(next) {
+                        var gen = new GenerateBundleSnapshots(track, missing, size);
+                        var bnpp = (gen.run.toPromise().array());
+                        status(2, true);
+                        bnpp.then(function(paths) {
+                            if (paths != null) {
+                                var item:BundleItem;
+                                for (path in paths) {
+                                    item = new BundleItem(this, path.name);
+                                    add( item );
+                                }
+                                status(3, true);
+                                defer(function() {
+                                    next();
+                                });
+                            }
+                            else {
+                                next('Expected Array<tannus.sys.Path>, got null');
+                            }
+                        }, next.raise());
+                    });
+
+                    defer(function() {
+                        exec();
+                    });
+                },
+                function(?error) {
+                    if (error != null) {
+                        //o.error( error );
+                        done( error );
+                    }
+                    else {
+                        status(4, true);
+                        //done();
+                    }
+                }
+            );
+            status(0, true);
+            done();
+        }
+
+        /**
+          'read' method for the returned stream
+         **/
+        function _read(o, len) {
+            if (status(0)) {
+                switch (_status.slice(1, 3)) {
+                    // all items buffered
+                    case [true, true, true]:
+                        o.done();
+
+                    case [true, _, false]:
+                        // do nothing, 'readable' will be dispatched when more items are buffered
+
+                    case other:
+                        //
+                }
+            }
+        }
+
+        var input = new StreamInput({
+            init: _init,
+            read: _read
+        });
+        input.pause();
+
+        return input;
+    }
+
+    /**
       * get a snapshot path
       */
     public function getSnapshotPath(time:Float, ?size:String):Promise<Path> {
@@ -185,7 +333,7 @@ class Bundle {
 
         if (_watcher == null) {
             _watcher = tannus.node.Fs.watch(path, function(type, filename) {
-                trace( filename );
+                //trace( filename );
                 if (isBundleItemName( filename )) {
                     var item:BundleItem = new BundleItem(this, subpath( filename ).name);
 
@@ -510,77 +658,82 @@ class Bundle {
     }
 
     /**
-      * get the Path to the bundle_info.json file
-      */
-    public function info_path():Path {
+      get the Path to the bundle_info file
+     **/
+    public inline function info_path():Path {
         return subpath( 'bundle_info.json' );
     }
 
     /**
       * parse and return the data from bundle_info.json
       */
-    public function get_info():Obj {
+    public function get_info():BundleInfoFileData {
         var path = info_path();
         if (Fs.exists( path )) {
-            return Json.parse(Fs.read( path ).toString());
+            try {
+                var data:BundleInfoFileData = cast Json.parse(Fs.read( path ).toString());
+                if (Reflect.hasField(data, 'default_thumb_time')) {
+                    data.defaultThumbTime = Reflect.field(data, 'default_thumb_time');
+                    Reflect.deleteField(data, 'thumb_times');
+                }
+                if (Reflect.hasField(data, 'thumbTimes')) {
+                    data.thumbTimes = Reflect.field(data, 'thumb_times');
+                    Reflect.deleteField(data, 'thumb_times');
+                }
+                return data;                
+            }
+            catch (e: Dynamic) {
+                Fs.deleteFile( path );
+                return get_info();
+            }
         }
         else {
-            var data = {
-                default_thumb_time: 0.0,
-                thumb_times: []
+            var data:BundleInfoFileData = {
+                defaultThumbTime: _defaultThumbTime(),
+                thumbTimes: _defaultThumbTimes()
             };
-            data.default_thumb_time = _defaultThumbTime();
-            data.thumb_times = _defaultThumbTimes();
-            return Obj.fromDynamic( data );
+            //data.default_thumb_time = _defaultThumbTime();
+            //data.thumb_times = _defaultThumbTimes();
+            //return Obj.fromDynamic( data );
+            return data;
         }
     }
 
     /**
       * encode [info] and write it to bundle_info.json
       */
-    public function set_info(info : Obj):Void {
-        var path = info_path();
-        var data = Json.stringify(info.toDyn(), null, '  ');
-        Fs.write(path, data);
-    }
-
-    /**
-      * get/set [info]
-      */
-    public function info(?newinfo : Obj):Obj {
-        if (newinfo == null) {
-            return get_info();
-        }
-        else {
-            set_info( newinfo );
-            return newinfo;
-        }
+    public function set_info(info : BundleInfoFileData):BundleInfoFileData {
+        //var path:Path = info_path();
+        //var data:String = Json.stringify( info );
+        //Fs.write(path, data);
+        Fs.write(info_path(), Json.stringify(info));
+        return info;
     }
 
     /**
       * read, manipulate, write [info]
       */
-    public function edit_info(edit:Obj->Void):Void {
-        var data = info();
+    public function edit_info(edit: BundleInfoFileData->Void):Void {
+        var data:BundleInfoFileData = get_info();
         edit( data );
-        info( data );
+        set_info( data );
     }
 
     /**
-      * set the 'default_thumb_time' property
-      */
-    public function setThumbTime(time : Float):Void {
+      set the time at which the thumbnail should be generated
+     **/
+    public function setThumbTime(time: Float):Void {
         edit_info(function(i) {
-            i['default_thumb_time'] = time;
+            i.defaultThumbTime = time;
         });
     }
 
     /**
-      * get/set the 'thumb_times' property
-      */
+      get/set the list of timestamps that thumbnails should be generated for
+     **/
     public function thumb_times(?value:Array<Float>, sort:Bool=true):Array<Float> {
         if (value == null) {
-            return info()['thumb_times'];
+            return get_info().thumbTimes;
         }
         else {
             edit_info(function(i) {
@@ -588,7 +741,7 @@ class Bundle {
                     value.sort( Reflect.compare );
                 }
 
-                i['thumb_times'] = value;
+                i.thumbTimes = value;
             });
             return value;
         }
@@ -598,25 +751,26 @@ class Bundle {
       * calculate the default thumb time
       */
     private function _defaultThumbTime():Float {
+        // create pseudo-random number generator
+        var rand:Random = new Random();
+
+        // get all snapshots
         var allsnaps = getAllSnapshots();
+
+        // if [allsnaps] not empty
         if (allsnaps.length > 0) {
-            var snap : BundleItem;
-            if (allsnaps.length == 1) {
-                snap = allsnaps[0];
-            }
-            else {
-                snap = RandomTools.choice( allsnaps );
-            }
+            var snap:BundleItem = (allsnaps.length == 1) ? allsnaps[0] : rand.choice(allsnaps);
             var time = snap.getTime();
             if (time != null) {
                 return time;
             }
         }
+
         if (track.data != null && track.data.meta != null && track.data.meta.duration != null) {
-            return (new Random().randfloat(0.0, track.data.meta.duration));
+            return (rand.randfloat(0.0, track.data.meta.duration));
         }
         else {
-            return 0.0;
+            return -1.0;
         }
     }
 
@@ -646,8 +800,8 @@ class Bundle {
                     throw error;
                 }
                 else if (dat != null) {
-                    var bundleInfo = info();
-                    var thumbTime:Float = bundleInfo.mget('default_thumb_time').or( 0 );
+                    var bundleInfo = get_info();
+                    var thumbTime:Float = bundleInfo.defaultThumbTime;
                     var snapp = getSnapshot(thumbTime, size);
                     @forward snapp;
                 }
@@ -681,9 +835,95 @@ class Bundle {
         }).array();
     }
 
+    /**
+      create and return a Stream of thumbnails
+     **/
     @:keep
-    public function betty() {
-        //
+    public function readThumbnails(size:String='15%', ?slice:{pos:Int, ?end:Int}):StreamInput<BundleItem> {
+        var times = thumb_times();
+        if (slice != null) {
+            times = times.slice(slice.pos, slice.end);
+        }
+
+        var ssi;
+        var input = new StreamInput({
+            init: function(o, done) {
+                ssi = streamMultipleSnapshots(times, size);
+                ssi.pause();
+                ssi.onEnd(function() {
+                    //o.done();
+                });
+                ssi.onError(function(error) {
+                    o.error( error );
+                });
+            },
+            read: function(o, len) {
+                o.next(ssi.read(len));
+            }
+        });
+        input.pause();
+        return input;
+    }
+
+    /**
+      test the stream interface
+     **/
+    @:keep
+    public function betty(?times:Iterable<TimeVal>, ?size:String):Void {
+        var _times: Array<Time>;
+        if (times != null) {
+            _times = times.array().map( resolveTime );
+        }
+        else {
+            _times = this.getAllSnapshots()
+                //.map(item -> item.getTime().toNonNullable())
+                .map.fn(_.getTime().toNonNullable())
+                .compact()
+                .map.fn(Time.fromFloat(_));
+        }
+
+        var tns = streamMultipleSnapshots(_times.map.fn(_.toFloat()), size);
+        tns.onReadable(function() {
+            trace("Stream is readable");
+            var tn:BundleItem;
+            do {
+                tn = tns.read(1);
+                trace( tn );
+            }
+            while (tn != null);
+        });
+        tns.onEnd(function() {
+            trace("Stream ended");
+        });
+        tns.onError(function(error) {
+            throw error;
+        });
+        tns.onClose(function(?error) {
+            trace('Stream closed ${error!=null?"with error:"+error:"without errors"}');
+        });
+        tns.onData(function(item) {
+            trace( item );
+        });
+        echo( tns );
+    }
+
+    /**
+      allow time-offsets to be provided in many different formats
+      all of which are resolved back to a Time instance by this method
+     **/
+    private function resolveTime<T:TimeVal>(t: T):Time {
+        var tv:TimeValue = TimeValue.fromAny( t );
+        if (tv.isAbsolute(true)) {
+            return tv.getTime();
+        }
+        else {
+            if (track.data != null && track.data.meta != null && track.data.meta.duration != null) {
+                return new Time(tv.getSecondsWith( track.data.meta.duration ));
+            }
+            else {
+                throw 'Cannot resolve relative/proportional time expressions until metadata is loaded';
+            }
+        }
     }
 
 /* === Static Methods === */
@@ -711,8 +951,13 @@ class Bundle {
     }
 
     public static function basicSizeDimensions(size : String):Dimensions {
-        return size.split('x').with([w, h], new Dimensions(Std.parseInt(w), Std.parseInt(h)));
+        return size.split('x')
+            .with([w, h], new Dimensions(Std.parseInt(w), Std.parseInt(h)));
     }
+
+/* === Computed Instance Fields === */
+
+    //public var info(get, set): BundleInfoFileData;
 
 /* === Instance Fields === */
 
@@ -724,5 +969,115 @@ class Bundle {
 
     // thumb-generation event emitter
     private var tgee : EventDispatcher;
+
+    // file-watcher object
     private var _watcher : Maybe<tannus.node.Fs.FSWatcher> = null;
 }
+
+/**
+  type of object stored in the bundle_info.json file
+ **/
+typedef BundleInfoFileData = {
+    var defaultThumbTime: Null<Float>;
+    var thumbTimes: Array<Float>;
+};
+
+abstract TimeValue (TimeValueType) from TimeValueType to TimeValueType {
+
+    public var type(get, never): TimeValueType;
+    private inline function get_type() return this;
+
+    public function isAbsolute(noop: Bool=false):Bool {
+        return noop ? type.match(TvTTime(_, null)) : type.match(TvTTime(_, _));
+    }
+
+    public inline function getSeconds():Float {
+        return switch type {
+            case TvTTime(secs, _): secs;
+            default: throw 'TypeError: Cannot extract seconds from $type';
+        };
+    }
+
+    public inline function getTime():Time {
+        return new Time(getSeconds());
+    }
+
+    public function getSecondsWith(base_seconds: Float):Float {
+        switch type {
+            case TvTTime(secs, null):
+                return secs;
+            case TvTTime(secs, op):
+                secs = (switch op {
+                    case Plus: abs(secs);
+                    case Minus: -abs(secs);
+                    case _: throw 'Wtf($type)';
+                });
+                return (base_seconds + secs);
+            case TvTPercent(perc, null):
+                return perc.of( base_seconds );
+            case TvTPercent(perc, op):
+                var secs:Float = perc.of( base_seconds );
+                secs = (switch op {
+                    case Plus: abs(secs);
+                    case Minus: -abs(secs);
+                    case _: throw 'Wtf($type)';
+                });
+                return (base_seconds + secs);
+            case _:
+                throw 'Wtf($type)';
+        }
+    }
+
+    public function getTimeWith(base: Time):Time {
+        return new Time(getSecondsWith(base.toFloat()));
+    }
+
+    @:from
+    public static function fromAny<T:TimeVal>(v: T):TimeValue {
+        return typeFromAny( v );
+    }
+
+    public static function typeFromAny<T:TimeVal>(value: T):TimeValueType {
+        if ((value is Float)) {
+            return TvTTime((value : Float));
+        }
+        else if ((value is Time)) {
+            return TvTTime((value : Time).toFloat());
+        }
+        else if ((value is Duration)) {
+            return TvTTime((value : Duration).toFloat());
+        }
+        else if ((value is String)) {
+            var sval:String = cast value;
+            var expr = TimeParser.run( sval );
+            switch expr {
+                case null:
+                    throw 'Unable to parse a Time value from "$sval"';
+
+                case ETime(time):
+                    return TvTTime(time.toFloat());
+
+                case ERel(op, ETime(_.toFloat()=>time)):
+                    return TvTTime(time, op);
+
+                case EPercent( percent ):
+                    return TvTPercent( percent );
+
+                case ERel(op, EPercent(percent)):
+                    return TvTPercent(percent, op);
+
+                case _:
+                    throw 'Unable to parse a Time value from $expr';
+            }
+        }
+        return null;
+    }
+}
+
+enum TimeValueType {
+    TvTTime(seconds:Float, ?rel:TimeOp);
+    TvTPercent(percent:Percent, ?rel:TimeOp);
+}
+
+typedef TimeVal = Either<Either<String, Float>, Either<Time, Duration>>;
+
