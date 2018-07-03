b889c66f636903c26475ad42747f7f60c2e06ada
DavisDevelopment <epikit@gmail.com>
2018-07-03 03:40:04 -0500

    reimplemented RepeatType enum as an abstract

--- src/pman/core/PlayerPlaybackProperties.hx
+++ src/pman/core/PlayerPlaybackProperties.hx
@@ -166,9 +166,16 @@ enum PPChange {
 	Scale(d : Delta<Float>);
 }
 
-enum RepeatType {
-	RepeatOff;
-	RepeatIndefinite;
-	RepeatOnce;
-	RepeatPlaylist;
+@:enum
+abstract RepeatType (Int) from Int to Int {
+    var RepeatOff = 0;
+    var RepeatIndefinite = 1;
+    var RepeatOnce = 2;
+    var RepeatPlaylist = 3;
 }
+//enum RepeatType {
+	//RepeatOff;
+	//RepeatIndefinite;
+	//RepeatOnce;
+	//RepeatPlaylist;
+//}
