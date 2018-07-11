a9fb7e8f11eb7f67c55c767567e233c244923b6d
DavisDevelopment <epikit@gmail.com>
2018-07-11 02:46:15 -0500

    created new MediaSort type

---
+++ src/pman/bg/media/MediaSort.hx
@@ -0,0 +1,10 @@
+package pman.bg.media;
+
+enum MediaSort {
+    MSTitle(ascending : Bool);
+    MSDuration(ascending: Bool);
+    MSDate(adscending : Bool);
+    MSViews(ascending : Bool);
+    MSRating(ascending : Bool);
+    MSNone;
+}
