729332cba502c9bb1ef9abf81742a59538901595
DavisDevelopment <epikit@gmail.com>
2018-07-17 01:33:58 -0500

    added some real structure to FluentTask

--- src/pman/async/tasks/FluentTask.hx
+++ src/pman/async/tasks/FluentTask.hx
@@ -7,9 +7,16 @@ import tannus.sys.FileSystem as Fs;
 import tannus.geom2.Area;
 import tannus.async.*;
 import tannus.node.Buffer;
+import tannus.node.Writable;
+import tannus.node.Duplex;
 import tannus.Nil;
 
 import ffmpeg.Fluent;
+
+import pman.async.Task1;
+
+import haxe.extern.EitherType as Either;
+import haxe.Constraints.Function;
 
 import Std.*;
 import tannus.math.TMath.*;
@@ -29,12 +36,72 @@ using tannus.async.Asyncs;
  **/
 class FluentTask extends Task1 {
     /* Constructor Function */
-    public function new():Void {
+    public function new(src: String):Void {
         super();
+
+        this.src = src;
+        this.dests = [];
+        fluent = Fluent.ffmpeg( src );
+
+        _build_();
     }
 
 /* === Instance Methods === */
 
+    public function addOutput(out: Either<String, Either<Writable, Duplex>>):FluentTask {
+        dests.push( out );
+    }
+
+    override function execute(done: VoidCb):Void {
+        fluent.onError(function(error, out, err) {
+            done( error );
+        });
+
+        fluent.onEnd(function() {
+            afterExecution(fluent, function(?error) {
+                done( error );
+            });
+        });
+
+        beforeExecution(fluent, function(?error) {
+            if (error != null)
+                done( error );
+            else {
+                fluent.execute();
+            }
+        });
+    }
+
+    function beforeExecution(f:Fluent, next:VoidCb) {
+        // bind event listeners
+        _bind_( f );
+
+        // add output destinations
+        for (x in dests) {
+            f.addOutput( x );
+        }
+
+        // continue
+        next();
+    }
+
+    function afterExecution(f:Fluent, next:VoidCb) {
+        next();
+    }
+
+    function _bind_(f: Fluent) {
+        f.onProgress(function(event) {
+            this.progress = event.percent;
+        });
+
+        f.onError(function(error, stdout, stderr) {
+            report( error );
+        });
+    }
+
 /* === Instance Fields === */
 
+    public var src: String = null;
+    public var dests: Array<Either<String, Either<Writable, Duplex>>>;
+    public var fluent: Fluent = null;
 }
