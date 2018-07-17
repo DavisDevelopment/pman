b4cdb1510f09b309515852b073d232c592e2a3e6
DavisDevelopment <epikit@gmail.com>
2018-07-17 02:23:01 -0500

    reimplemented the PlaylistCommand class

--- src/pman/pmbash/commands/PlaylistCommand.hx
+++ src/pman/pmbash/commands/PlaylistCommand.hx
@@ -12,7 +12,9 @@ import pman.format.pmsh.Expr;
 import pman.format.pmsh.Cmd;
 import pman.pmbash.commands.*;
 
-import electron.Tools.*;
+//import electron.Tools.*;
+import edis.Globals.*;
+import pman.Globals.*;
 
 using StringTools;
 using tannus.ds.StringUtils;
@@ -21,7 +23,75 @@ using tannus.ds.ArrayTools;
 using Slambda;
 using pman.async.VoidAsyncs;
 
-class PlaylistCommand extends Command {
+/**
+  command-set for working with PMan's playlist system
+ **/
+class PlaylistCommand extends HierCommand {
+    override function _build_() {
+        inline function cmd(self:HierCommand, name, f) self.subCmd(name, onlyArgs(f));
+        function pcmd(self:HierCommand, name, f) {
+            self.createSubCommand(name, {}, function(c) {
+                c.pythonicMain( f );
+            });
+        }
+
+        cmd(this, 'export', cmdExport);
+        cmd(this, 'save', cmdSave);
+        cmd(this, 'open, load', cmdOpen);
+        cmd(this, 'shuffle', cmdShuffle);
+        cmd(this, 'delete', cmdDelete);
+    }
+
+    function cmdExport(argv:Array<CmdArg>, done:VoidCb) {
+        player.exportPlaylist(function() {
+            done();
+        });
+    }
+
+    function cmdSave(argv:Array<CmdArg>, done:VoidCb) {
+        var name:String = null;
+        if (!argv.empty())
+            name = argumentString(argv[0]);
+        player.savePlaylist(null, name, function() {
+            done();
+        });
+    }
+
+    function cmdOpen(argv:Array<CmdArg>, done:VoidCb) {
+        if (argv.empty()) {
+            done();
+        }
+        else {
+            player.loadPlaylist(argumentString(argv[0]), function() {
+                done();
+            });
+        }
+    }
+
+    function cmdShuffle(argv:Array<CmdArg>, done:VoidCb) {
+        player.shufflePlaylist();
+        done();
+    }
+
+    function cmdDelete(argv:Array<CmdArg>, done:VoidCb) {
+        if (argv.empty()) {
+            done();
+        }
+        else {
+            try {
+                var plfile = player.app.appDir.playlistFile(argumentString(argv[0]));
+                plfile.delete();
+                done();
+            }
+            catch (err: Dynamic) {
+                report( err );
+                done();
+            }
+        }
+    }
+}
+
+class OldPlaylistCommand extends Command {
     override function execute(i:Interpreter, argv:Array<CmdArg>, done:VoidCb):Void {
         var args = argv.map.fn( _.value );
         var action:String = args.shift();
