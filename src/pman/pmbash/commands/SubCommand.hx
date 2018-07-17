976786e726bc7d1c2c4e3a945cfad5ac1052ab03
DavisDevelopment <epikit@gmail.com>
2018-07-17 02:15:57 -0500

    created new SubCommand class

---
+++ src/pman/pmbash/commands/SubCommand.hx
@@ -0,0 +1,37 @@
+package pman.pmbash.commands;
+
+import tannus.io.*;
+import tannus.ds.*;
+import tannus.ds.tuples.Tup2;
+import tannus.async.*;
+import tannus.math.*;
+import tannus.sys.Path;
+import tannus.TSys as Sys;
+
+import pman.core.*;
+import pman.media.*;
+import pman.bg.media.*;
+import pman.async.tasks.*;
+import pman.format.pmsh.*;
+import pman.format.pmsh.Token;
+import pman.format.pmsh.Expr;
+import pman.format.pmsh.Cmd;
+import pman.pmbash.commands.*;
+import pman.pmbash.args.*;
+
+import Slambda.fn;
+import tannus.math.TMath.*;
+import edis.Globals.*;
+import pman.Globals.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Lambda;
+using tannus.ds.ArrayTools;
+using Slambda;
+using tannus.FunctionTools;
+using tannus.async.Asyncs;
+
+class SubCommand extends HierCommand {
+
+}
